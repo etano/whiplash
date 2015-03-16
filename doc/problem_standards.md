803d0b85544a34c5a8c0bd3c47f18faaaaffb1f1
Ilia Zintchenko <zintchenko@itp.phys.ethz.ch>
2015-03-16 13:21:26 +0100

    updated scheduler

--- doc/problem_standards.md
+++ doc/problem_standards.md
@@ -10,11 +10,29 @@
 # SAT
 
 ## Input
-    # comments
-    varA0 varA1 ...
-    varB0 varB1 ...
-    ...
-    
+
+Weigthed Partial Max-SAT input format
+
+In Weigthed Partial Max-SAT, the parameters line is "p wcnf nbvar
+nbclauses top". We associate a weight with each clause, wich is the
+first integer in the clause. Weigths must be greater than or equal to
+1, and the sum of all soft clauses smaller than 263. Hard clauses have
+weigth top and soft clauses have a weigth smaller than top. We assure
+that top is a weight always greater than the sum of the weights of
+violated soft clauses.
+
+Example of Weigthed Partial Max-SAT formula:
+
+c
+c comments Weigthed Partial Max-SAT
+c
+p wcnf 4 5 16
+16 1 -2 4 0
+16 -1 -2 3 0
+8 -2 -4 0
+4 -3 2 0
+3 1 3 0
+
 ## Output
 
 # QUBO/Ising
--- src/scheduler_mpi_simple.cpp
+++ src/scheduler_mpi_simple.cpp
@@ -4,7 +4,23 @@
 #include <fstream>
 #include <iostream>
 #include <unistd.h>
+#include <dirent.h>
 #include <mpi.h>
+
+inline bool empty_dir(const char* dir_name)
+{
+  DIR* dir(opendir(dir_name));
+  assert(dir != NULL);
+  unsigned n(0);  
+  while(readdir(dir) != NULL){
+    if(++n > 2){
+      closedir(dir);
+      return false;
+    }
+  }
+  closedir(dir);
+  return true;
+}
 
 int main(int argc, char* argv[])
 {
@@ -12,7 +28,7 @@ int main(int argc, char* argv[])
   MPI_Init(&argc, &argv);
   MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
 
-  while(true){
+  while(!empty_dir("run")){
     const std::string command = "file=$(ls run |sort -R | head -n 1) && command=$(cat run/${file}) && rm run/${file} && ${command}";
     system(command.c_str());
     usleep(1e05 * world_rank);
