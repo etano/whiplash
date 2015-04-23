326190439cbba1d7cda7e3f5e66982c0714678a7
Alex Kosenkov <alex.kosenkov@gmail.com>
2015-04-23 15:22:29 +0200

    Cleaning #2

---
+++ src/entities/qubo/qubo.hpp
@@ -0,0 +1,14 @@
+#ifndef SIMFW_ENTITIES_QUBO_HPP
+#define SIMFW_ENTITIES_QUBO_HPP
+
+namespace simfw { namespace entities {
+
+    class qubo : public abstract_dynamic_problem {
+    public:
+        virtual ~qubo(){}
+    };
+
+} }
+
+#endif
+
