c7db7c171593dc65b5d4bb8bfcfbc078759f019b
Alex Kosenkov <alex.kosenkov@gmail.com>
2015-04-23 15:43:27 +0200

    wrong namespace

--- src/entities/ising/ising.hpp
+++ src/entities/ising/ising.hpp
@@ -1,7 +1,7 @@
 #ifndef SIMFW_ENTITIES_ISING_HPP
 #define SIMFW_ENTITIES_ISING_HPP
 
-namespace simfw { namespace types {
+namespace simfw { namespace entities {
 
     class ising : public abstract_dynamic_problem {
     public:
