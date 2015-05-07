e8dc9974f0ddbee99bf990e5a665e2439273a563
Alex Kosenkov <alex.kosenkov@gmail.com>
2015-05-07 17:05:10 +0200

    renamed db-collection names

--- src/deployment/basic.hpp
+++ src/deployment/basic.hpp
@@ -5,8 +5,8 @@ namespace wdb { namespace deployment {
 
     basic::basic(odb::iobjectdb& db)
         : db(db),
-        instances( db.provide_collection("instances") ),
-        hamiltonians( db.provide_collection("hamiltonians") )
+        instances( db.provide_collection("properties") ),
+        hamiltonians( db.provide_collection("models") )
     {
     }
 
@@ -18,14 +18,14 @@ namespace wdb { namespace deployment {
         odb::mongo::object serialized, serialized_state;
         entities::generic::instance pb( hid, solver, params );
         pb.serialize_state(serialized_state);
-        pb.serialize(db.get_next_id("instances"), serialized, serialized_state);
+        pb.serialize(db.get_next_id("properties"), serialized, serialized_state);
         static_cast<odb::mongo::collection&>(instances).insert( serialized );
     }
 
     void basic::insert_hamil(std::ifstream& in){
         odb::mongo::object serialized;
         entities::generic::hamil H( in );
-        H.serialize(db.get_next_id("hamiltonians"), serialized);
+        H.serialize(db.get_next_id("models"), serialized);
         static_cast<odb::mongo::collection&>(hamiltonians).insert( serialized ); 
     }
 
