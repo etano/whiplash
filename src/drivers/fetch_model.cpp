56e0252a9bfdb66f66c431360aa9d0b0dd010e9b
Alex Kosenkov <alex.kosenkov@gmail.com>
2015-05-26 21:07:33 +0200

    reverting pkg-config detail in Make (+ updated indices in drivers)

--- src/drivers/fetch_model.cpp
+++ src/drivers/fetch_model.cpp
@@ -5,7 +5,7 @@ int main(int, char**){
     objectdb db("cwave.ethz.ch:27017");
     wdb::deployment::basic sf(db);
 
-    sf.fetch_model(18);
+    sf.fetch_model(2);
     return 0;
 }
 
--- src/drivers/worker.cpp
+++ src/drivers/worker.cpp
@@ -8,8 +8,8 @@ int main(int argc, char** argv){
     void** params = (void**)malloc(sizeof(void*)*std::max(10,argc));
     for(int i = 0; i < argc; i++) params[i] = argv[i];
 
-    auto H(sf.fetch_model(18));    params[1] = &H;
-    auto I(sf.fetch_property(17)); params[2] = &I;
+    auto H(sf.fetch_model(2));    params[1] = &H;
+    auto I(sf.fetch_property(3)); params[2] = &I;
 
     auto& test = sf.load("test.app");
     test(argc, (char**)params);
