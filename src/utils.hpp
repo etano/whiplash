d788e9f89062ea92cf5ffd25cd6ff1532d44de90
Ethan Brown <ethan.w.brown@gmail.com>
2015-06-22 18:39:27 +0200

    forgot to add utils.hpp

---
+++ src/utils.hpp
@@ -0,0 +1,24 @@
+#ifndef WDB_UTILS_HPP
+#define WDB_UTILS_HPP
+
+namespace wdb {
+
+  std::map<std::string,std::string> parse_args(int argc, char *argv[]){
+        std::map<std::string,std::string> params;
+        bool have_key = false;
+        std::string key;
+        for(int i = 1; i < argc; ++i){
+            if(argv[i][0] == '-'){
+                if(!have_key) have_key = true;
+                key = argv[i] + 1;
+            }else if(have_key){
+                params[key] = std::string(argv[i]);
+                have_key = false;
+            }
+        }
+        return params;
+    }
+
+}
+
+#endif
