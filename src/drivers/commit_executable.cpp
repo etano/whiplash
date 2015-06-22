7d477b673841d3ac2da2e0b4b041f3bee1461f63
Ethan Brown <ethan.w.brown@gmail.com>
2015-06-22 17:42:06 +0200

    updated param parsing

--- src/drivers/commit_executable.cpp
+++ src/drivers/commit_executable.cpp
@@ -1,5 +1,5 @@
 #include "wdb.hpp"
-#include "odb/mongo/utils.hpp"
+#include "utils.hpp"
 
 using wdb::odb::mongo::objectdb;
 
@@ -7,11 +7,9 @@ int main(int argc, char* argv[]){
     objectdb db("cwave.ethz.ch:27017");
     wdb::deployment::basic sf(db);
 
-    auto doc = wdb::odb::mongo::parse_args(argc,argv); // please, clean up me
-    std::string file_name( doc.view()["file"].get_utf8().value );
-    std::string model_class( doc.view()["class"].get_utf8().value );
+    auto params = wdb::parse_args(argc,argv);
 
-    sf.insert_executable(file_name,model_class);
+    sf.insert_executable(params["file"],params["class"]);
 
     return 0;
 }
