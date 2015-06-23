911352099f01a7b5dbacf0681549b8d43dba4e85
Alex Kosenkov <alex.kosenkov@gmail.com>
2015-06-23 19:52:14 +0200

    a few remaining v/o

--- src/entities/dynamic_generic/model.hpp
+++ src/entities/dynamic_generic/model.hpp
@@ -6,13 +6,9 @@ namespace wdb { namespace entities { namespace dynamic_generic {
     class model : public wdb::entities::generic::model {
     public:
         model(std::string model_class, std::ifstream& in) : wdb::entities::generic::model(model_class,in) {}
-
         model(std::string model_class, const odb::iobject& o) : wdb::entities::generic::model(model_class,o) {}
-
         virtual ~model() override {};
-
-        virtual void print() { std::cout << "print not defined" << std::endl; }
-
+        virtual void print() override { std::cout << "print not defined" << std::endl; }
         virtual void serialize_configuration(odb::iobject& configuration) override {}
     };
 
