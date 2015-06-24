a218e7f83ff185cc9dfd83a16e48d8fcf257fedd
Alex Kosenkov <alex.kosenkov@gmail.com>
2015-06-24 23:01:17 +0200

    adding test.sh + rename

--- src/demo.sh
+++ src/demo.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 echo 'Initializing WDB...'
-./drivers/init_db.driver
+$1 ./drivers/init_db.driver
 echo 'Committing model...'
-./drivers/commit_model.driver -file apps/hamil -class ising
+$1 ./drivers/commit_model.driver -file apps/hamil -class ising
 echo 'Committing executable...'
-./drivers/commit_executable.driver -file apps/test.app -class ising
+$1 ./drivers/commit_executable.driver -file apps/test.app -class ising
 echo 'Committing property...'
-./drivers/commit_property.driver apps/tasks
+$1 ./drivers/commit_property.driver apps/tasks
 echo 'Resolving properties...'
-./drivers/resolve_properties.driver
+$1 ./drivers/resolve_properties.driver
---
+++ src/test.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+echo 'Running test suites selection...'
+$1 tests/property.test
