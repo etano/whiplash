93a933aed35eac4125bd70f543ac0b20eba74028
Ethan Brown <ethan.w.brown@gmail.com>
2015-07-28 16:38:12 +0200

    added a second demo script and associated tasks for anc sa solver

---
+++ src/demo2.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo 'Initializing WDB...'
+$1 ./drivers/init_db.driver
+echo 'Committing model...'
+$1 ./drivers/commit_model.driver -file apps/hamil -class ising -owner akosenko
+echo 'Committing executable...'
+$1 ./drivers/commit_executable.driver -file /Users/ethan/src/anc/an_ss_ge_fi_vdeg -class ising -description "desc" -algorithm "algo" -configuration "conf" -version "1.0" -build "O3" -owner akosenko
+echo 'Committing property...'
+$1 ./drivers/commit_property.driver apps/tasks
+echo 'Querying property...'
+$1 ./drivers/query.driver
+echo 'Querying property again...'
+$1 ./drivers/query.driver
