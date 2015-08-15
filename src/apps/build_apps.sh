e9f2ab19d6a90132d421c3beecceddba8d304a7d
Ethan Brown <ethan.w.brown@gmail.com>
2015-08-15 14:47:02 -0600

    force apps to rebuild in build_apps script

--- src/apps/build_apps.sh
+++ src/apps/build_apps.sh
@@ -2,12 +2,15 @@
 
 git submodule update --init
 export WDB_HOME=$(pwd)/../../
-cd anc && make -j single
+cd anc && make clean && make -j single
 cd ../
 cd spin_glass_solver
 if [ ! -d "build" ]; then
   mkdir build
+else
+  rm -rf build
+  mkdir build
 fi
 cd build/ && cmake .. && make -j && make install
 cd ../../
-cd DT-SQA && make -j && cd ../
+cd DT-SQA && make clean && make -j && cd ../
