8dbd2786ed0a01e7c8b749780ca0f833585744bf
Alex Kosenkov <alex.kosenkov@gmail.com>
2015-09-11 18:34:21 +0200

    updated clean_apps

--- src/apps/clean_apps.sh
+++ src/apps/clean_apps.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+cd DT-SQA && make clean && cd ..
+cd XXcode && make -f Makefile.wdb clean && cd ..
 cd anc && make clean; cd ..
 rm -rf spin_glass_solver/build
-cd DT-SQA && make clean && cd ..
