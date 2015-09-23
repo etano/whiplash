18abb33069499dbc8c174495359e9139b38326d2
Alex Kosenkov <alex.kosenkov@gmail.com>
2015-09-23 22:32:39 +0200

    putting back custom worker example

--- src/apps/drivers/worker.cpp
+++ src/apps/drivers/worker.cpp
@@ -6,7 +6,21 @@ int main(int argc, char* argv[]){
 
     auto controller = wdb::entities::factory::make<framework::e::controller>("generic");
     root.add_controller( *controller );
-    root.yield();
 
+    /*
+    // custom (!) worker example //
+    auto ising_controller = wdb::entities::factory::make<framework::e::controller>("ising");
+    auto sat_controller   = wdb::entities::factory::make<framework::e::controller>("sat");
+    auto xx_controller    = wdb::entities::factory::make<framework::e::controller>("xx");
+
+    root.add_controller( *xx_controller ); // TOFIX: pull method should be controller-specific
+    root.add_controller( *ising_controller );
+    root.add_controller( *sat_controller );
+
+    root.declare_segue( *sat_controller, *ising_controller );
+    root.declare_segue( *ising_controller, *sat_controller );
+    */
+
+    root.yield();
     return 0;
 }
