0b636258f41e413e26304cf1fe364d64562c5a5a
Alex Kosenkov <kosenkov@itp.phys.ethz.ch>
2015-09-25 22:11:34 +0200

    adding docker (to be continued)

---
+++ build/boot.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# Whiplash container types:
+# wdb-rte-container : contains node run-time environment
+# wdb-www-container : contains web-interface for queries
+# wdb-odb-container : contains mongo database
+
+
+docker run --name wdb-odb -p 0.0.0.0:27017:27017 -d mongo:3.1.8
+
+docker build -t wdb-rte-container -f Dockerfile.rte ..
+# TODO:
+# needs to contain compiled rte and binaries and then connect to mongo via 
+# $MONGO_PORT_27017_TCP_ADDR:$MONGO_PORT_27017_TCP_PORT 
+# commiting new executables here as well
+docker run -d -P --name wdb-rte --link wdb-odb:mongo wdb-rte-container
+
+
+
+
+docker build -t wdb-www-container -f Dockerfile.www ..
+docker run -d -P --name wdb-www --link wdb-odb:mongo -v $PWD/../www:/var/www/html wdb-www-container
+alias mongo="docker run -it --link wdb-odb:mongo --rm mongo sh -c 'exec mongo \"\$MONGO_PORT_27017_TCP_ADDR:\$MONGO_PORT_27017_TCP_PORT\"'"
+
---
+++ www/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head> 
+    <title>Whiplash DB</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta name="apple-mobile-web-app-capable" content="yes">
+    <link rel="stylesheet" href="css/main.css" type="text/css" />
+    <script src="scripts/jquery/jquery-2.1.1.min.js"></script>
+    <script src="scripts/main.js"></script>
+</head> 
+<body>
+    <section id="logo">Whiplash DB</section>
+    <section id="query">
+    </section>
+</body>
+</html>
