7ceaca9c47a67e159929d3eaebec71879d13b332
Alexandr Kosenkov <alex@Alexandrs-MacBook.local>
2015-09-26 19:15:24 +0200

    renaming build to docker

--- docker/boot.sh
+++ docker/boot.sh
@@ -5,7 +5,11 @@
 # wdb-odb-container : contains mongo database
 
 
-docker run --name wdb-odb -p 0.0.0.0:27017:27017 -d mongo:3.1.8
+docker run --name wdb-odb -p 0.0.0.0:27017:27017 -d mongo:latest
+
+docker build -t wdb-www-container -f Dockerfile.www ..
+docker run -d -P --name wdb-www --link wdb-odb:mongo -v $PWD/../www:/var/www/html wdb-www-container
+alias mongo="docker run -it --link wdb-odb:mongo --rm mongo sh -c 'exec mongo \"\$MONGO_PORT_27017_TCP_ADDR:\$MONGO_PORT_27017_TCP_PORT\"'"
 
 docker build -t wdb-rte-container -f Dockerfile.rte ..
 # TODO:
@@ -13,11 +17,4 @@ docker build -t wdb-rte-container -f Dockerfile.rte ..
 # $MONGO_PORT_27017_TCP_ADDR:$MONGO_PORT_27017_TCP_PORT 
 # commiting new executables here as well
 docker run -d -P --name wdb-rte --link wdb-odb:mongo wdb-rte-container
-
-
-
-
-docker build -t wdb-www-container -f Dockerfile.www ..
-docker run -d -P --name wdb-www --link wdb-odb:mongo -v $PWD/../www:/var/www/html wdb-www-container
-alias mongo="docker run -it --link wdb-odb:mongo --rm mongo sh -c 'exec mongo \"\$MONGO_PORT_27017_TCP_ADDR:\$MONGO_PORT_27017_TCP_PORT\"'"
 
