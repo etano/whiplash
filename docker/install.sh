ac31002f4b8671dfebd6fd59d32b3f9105c1c9ae
Alexandr Kosenkov <kosenkov@itp.phys.ethz.ch>
2015-09-26 23:03:16 +0200

    docker container for building now works

--- docker/install.sh
+++ docker/install.sh
@@ -4,17 +4,12 @@
 # wdb-www-container : contains web-interface for queries
 # wdb-odb-container : contains mongo database
 
-
 docker run --name wdb-odb -p 0.0.0.0:27017:27017 -d mongo:latest
 
 docker build -t wdb-www-container -f Dockerfile.www ..
 docker run -d -P --name wdb-www --link wdb-odb:mongo -v $PWD/../www:/var/www/html wdb-www-container
 alias mongo="docker run -it --link wdb-odb:mongo --rm mongo sh -c 'exec mongo \"\$MONGO_PORT_27017_TCP_ADDR:\$MONGO_PORT_27017_TCP_PORT\"'"
 
+cd ../depends/mongo-c-driver && git submodule update --init && cd ../../docker
 docker build -t wdb-rte-container -f Dockerfile.rte ..
-# TODO:
-# needs to contain compiled rte and binaries and then connect to mongo via 
-# $MONGO_PORT_27017_TCP_ADDR:$MONGO_PORT_27017_TCP_PORT 
-# commiting new executables here as well
-docker run -d -P --name wdb-rte --link wdb-odb:mongo wdb-rte-container
-
+docker run -it -P --name wdb-rte --link wdb-odb:mongo --rm -v $PWD/../:/usr/src/wdb/mount wdb-rte-container
