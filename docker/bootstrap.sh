d0cc13229132f0026fbc59925e89c4e141000fb6
Alexandr Kosenkov <kosenkov@itp.phys.ethz.ch>
2015-09-27 00:03:58 +0200

    improving docker structure

--- docker/bootstrap.sh
+++ docker/bootstrap.sh
@@ -1,15 +1,25 @@
 #!/bin/bash
-# Whiplash container types:
-# wdb-rte-container : contains node run-time environment
-# wdb-www-container : contains web-interface for queries
-# wdb-odb-container : contains mongo database
 
+
+# WDB-ODB-CONTAINER : contains mongo database
 docker run --name wdb-odb -p 0.0.0.0:27017:27017 -d mongo:latest
 
+
+# WDB-WWW-CONTAINER : contains web-interface for queries
 docker build -t wdb-www-container -f Dockerfile.www ..
 docker run -d -P --name wdb-www --link wdb-odb:mongo -v $PWD/../www:/var/www/html wdb-www-container
 alias mongo="docker run -it --link wdb-odb:mongo --rm mongo sh -c 'exec mongo \"\$MONGO_PORT_27017_TCP_ADDR:\$MONGO_PORT_27017_TCP_PORT\"'"
 
+
+# WDB-BUILD-CONTAINER : contains all dependencies compiled
 cd ../depends/mongo-c-driver && git submodule update --init && cd ../../docker
-docker build -t wdb-rte-container -f Dockerfile.rte ..
-docker run -it -P --name wdb-rte --link wdb-odb:mongo --rm -v $PWD/../:/usr/src/wdb/mount wdb-rte-container
+docker build -t wdb-build-container -f Dockerfile.build ..
+
+# make drivers
+docker run -it -P --name wdb-build --link wdb-odb:mongo --rm -v $PWD/../:/usr/src/wdb/mount wdb-build-container sh -c "cd mount/src; make"
+# make apps
+docker run -it -P --name wdb-build --link wdb-odb:mongo --rm -v $PWD/../:/usr/src/wdb/mount wdb-build-container sh -c "cd mount/src/apps; ./build_apps.sh"
+
+
+# WDB-SCHEDULER-CONTAINER : runs scheduler + workers
+# ...
