48dea174b98cfa63668362932a41e16212f65db3
Alexandr Kosenkov <kosenkov@itp.phys.ethz.ch>
2015-09-27 02:18:41 +0200

    docker push

--- docker/push.sh
+++ docker/push.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-# todo
+docker build -t whiplash/www:experimental -f Dockerfile.www ..
+docker push whiplash/www
+
+docker build -t whiplash/rte-local:deploy -f Dockerfile.deploy ..
+docker push whiplash/rte-local
+
