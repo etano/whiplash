2ea72e46a25603e51002042463b653cd9018432d
Alexandr Kosenkov <kosenkov@itp.phys.ethz.ch>
2015-09-27 02:12:51 +0200

    minor

--- docker/build.sh
+++ docker/build.sh
@@ -3,12 +3,12 @@
 
 # WDB-ODB-CONTAINER : contains mongo database
 docker run --name wdb-odb -p 0.0.0.0:27017:27017 -d mongo:latest
+alias mongo="docker run -it --link wdb-odb:mongo --rm mongo sh -c 'exec mongo \"\$MONGO_PORT_27017_TCP_ADDR:\$MONGO_PORT_27017_TCP_PORT\"'"
 
 
 # WDB-WWW-CONTAINER : contains web-interface for queries
 docker build -t wdb-www-container -f Dockerfile.www ..
 docker run -d -P --name wdb-www --link wdb-odb:mongo wdb-www-container
-alias mongo="docker run -it --link wdb-odb:mongo --rm mongo sh -c 'exec mongo \"\$MONGO_PORT_27017_TCP_ADDR:\$MONGO_PORT_27017_TCP_PORT\"'"
 
 
 # WDB-BUILD-CONTAINER : contains all dependencies compiled
@@ -20,6 +20,3 @@ docker run -it -P --name wdb-build --link wdb-odb:mongo --rm -v $PWD/../:/usr/sr
 # make apps
 docker run -it -P --name wdb-build --link wdb-odb:mongo --rm -v $PWD/../:/usr/src/wdb/mount wdb-build-container sh -c "cd mount/src/apps; ./build_apps.sh"
 
-
-# WDB-SCHEDULER-CONTAINER : runs scheduler + workers
-# ...
