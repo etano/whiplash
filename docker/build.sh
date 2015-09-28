17c9e4e549787bfd39c3054a8f696f82abecc2ba
Alex Kosenkov <kosenkov@itp.phys.ethz.ch>
2015-09-28 20:16:51 +0200

    adding WDB_HOME to build.sh

--- docker/build.sh
+++ docker/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-docker run -it -P --name wdb-build --rm -v $PWD/../:/usr/src/wdb/mount whiplash/rte-local:build sh -c "cd mount/src; make"
-docker run -it -P --name wdb-build --rm -v $PWD/../:/usr/src/wdb/mount whiplash/rte-local:build sh -c "cd mount/src/apps; ./build_apps.sh"
+docker run -it -P --name wdb-build --rm -v $PWD/../:/usr/src/wdb/mount whiplash/rte-local:build sh -c "cd mount/src; WDB_HOME=/usr/src/wdb/mount make"
+docker run -it -P --name wdb-build --rm -v $PWD/../:/usr/src/wdb/mount whiplash/rte-local:build sh -c "cd mount/src/apps; WDB_HOME=/usr/src/wdb/mount ./build_apps.sh"
 
