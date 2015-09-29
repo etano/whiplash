f2b2007cf08a58e86f3d9f89e7782b6518bbd118
Ethan Brown <ethan.w.brown@gmail.com>
2015-09-29 14:37:10 +0200

    finishing python scripts for demo

--- src/tests/demo_python_simple.py
+++ src/tests/demo_python_simple.py
@@ -1,5 +1,5 @@
 #
-# This demo script fetches models froms the database, registers a solver, form property requests, runs the solver on them locally, and finally pushes back resolved properties
+# This demo script fetches models froms the database, registers a solver, form property requests, runs the solver on them locally, pushes back resolved properties, and finally plots a histogram of results
 #
 
 # Make WhiplashDB instance
@@ -7,29 +7,45 @@ import sys,os
 wdb_home = os.environ.get('WDB_HOME')
 sys.path.append(wdb_home+'lib/python')
 import whiplashdb
-wdb = whiplashdb.wdb(wdb_home,"whiplash.ethz.ch:27017")
+from subprocess import Popen, PIPE
+import json
+wdb = whiplashdb.wdb(wdb_home,"localhost:27017")
 
 # Fetch models
-print 'Fetching models'
-model_filter = {'class':'ising','owner':'ebrown','lattice_type':'random','coupling_type':'gaussian'}
-model_ids = wdb.FetchModels(model_filter)
+print 'Querying for models'
+model_filter = {'class':'ising','owner':'ebrown','params.lattice_type':'random','params.coupling_type':'gaussian'}
+model_ids = wdb.QueryModels(model_filter)
+print model_ids
 
 # Register solver
+print 'Registering solver'
 executable = {'class':'ising','owner':'ebrown','description':'foo','algorithm':'SA','version':'bar','build':'O3','schedule':'linear','path':wdb_home+'/bin/test.app'}
 executable_id = wdb.CommitExecutable(executable)
+print executable_id
 
-# Form property requests
-print 'Form property requests'
-property = {'class':'ising','owner':'ebrown','executable':executable_id,'n_sweeps':10,'T_0':10.0,'T_1':1e-8}
-property_json = wdb.FormProperties(property, model_ids, n_reps=10000)
-
-# Call solver to resolve properties
+# Form property requests and solve them on local solver
+print 'Form property requests and solve them'
 properties = []
-for p in property_json:
-    # Call solver with argument p
-    properties.append(  solver(p)  ) # FIXME: use subprocess or whatever
+for model_id in model_ids:
+    property = {'class':'ising','owner':'ebrown','executable_id':executable_id,'model_id':model_id,'n_sweeps':10,'T_0':10.0,'T_1':1e-8}
+    model = wdb.FetchModel(model_id)
+    executable = wdb.FetchExecutable(executable_id)
+    for i in range(10000):
+        # Packaged executables (SA, SQA, UE) take JSON input and give JSON output
+        # though in general user is free to do this how they please
 
-# Commit properties
+        # Write JSON file
+        input_path = executable['path']+'.tmp.json'
+        f = open(input_path,'w')
+        f.write(json.dumps({'property':property,'model':model}))
+
+        # Run solver
+        print executable['path'], input_path
+        p = Popen([executable['path'],input_path],stdout=PIPE,stderr=PIPE,bufsize=1)
+        (stdout, stderr) = p.communicate()
+        properties.append(json.loads(stdout))
+
+# Commit resolved properties
 property_ids = wdb.CommitProperties(properties)
 
 # Form query
