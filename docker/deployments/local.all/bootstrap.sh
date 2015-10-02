d4249c9f063863689a02a75062bb0bdceb81298f
root <root@phys.ethz.ch>
2015-10-02 20:11:51 +0200

    fixing local.all mongo deployment

--- docker/deployments/local.all/bootstrap.sh
+++ docker/deployments/local.all/bootstrap.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # Mongo
-docker run --name wdb-odb -p 0.0.0.0:27017:27017 -d mongo:latest
+docker run --name wdb-odb -v /data/db:/data/db -p 27017:27017 -d mongo:latest
 alias mongo="docker run -it --link wdb-odb:mongo --rm mongo sh -c 'exec mongo \"\$MONGO_PORT_27017_TCP_ADDR:\$MONGO_PORT_27017_TCP_PORT\"'"
 
 # Web-interface
-docker run -p 80:80  -v /var/www/storage:/var/www/html/uploads -d -P --name wdb-www whiplash/www:experimental
+docker run --name wdb-www whiplash/www:experimental --link wdb-odb:mongo -p 80:80 -v /var/www/storage:/var/www/html/uploads -d -P
 
 # Run-time
-docker run -d -P --name wdb-rte --link wdb-odb:mongo whiplash/rte-local:deploy sh -c "./drivers/scheduler.driver -dbhost \$MONGO_PORT_27017_TCP_ADDR:\$MONGO_PORT_27017_TCP_PORT"
+docker run --name wdb-rte --link wdb-odb:mongo whiplash/rte-local:deploy -d -P sh -c "./drivers/scheduler.driver -dbhost \$MONGO_PORT_27017_TCP_ADDR:\$MONGO_PORT_27017_TCP_PORT"
