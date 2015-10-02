beadd8a0af65c033e950d2fcfcb09d8c14d9aa39
root <root@phys.ethz.ch>
2015-10-02 22:11:30 +0200

    changed it back to d

--- docker/deployments/local.all/bootstrap.sh
+++ docker/deployments/local.all/bootstrap.sh
@@ -5,7 +5,7 @@ docker run --name wdb-odb -v /data/db:/data/db -p 27017:27017 -d mongo:latest
 alias mongo="docker run -it --link wdb-odb:mongo --rm mongo sh -c 'exec mongo \"\$MONGO_PORT_27017_TCP_ADDR:\$MONGO_PORT_27017_TCP_PORT\"'"
 
 # Web-interface
-docker run --link wdb-odb:mongo --name wdb-www -p 80:80 -v /var/www/html:/var/www/html -v /var/www/storage:/var/www/html/uploads -it -P -t whiplash/www:experimental
+docker run --link wdb-odb:mongo --name wdb-www -p 80:80 -v /var/www/html:/var/www/html -v /var/www/storage:/var/www/html/uploads -d -P -t whiplash/www:experimental
 
 # Run-time
 docker run --name wdb-rte --link wdb-odb:mongo whiplash/rte-local:deploy -d -P sh -c "./drivers/scheduler.driver -dbhost \$MONGO_PORT_27017_TCP_ADDR:\$MONGO_PORT_27017_TCP_PORT"
