22258847e85ed1cbb321de1194e576094752c632
root <root@phys.ethz.ch>
2015-10-02 19:16:42 +0200

    deployment on virtual machine seems good

--- docker/deployments/local.all/bootstrap.sh
+++ docker/deployments/local.all/bootstrap.sh
@@ -5,7 +5,7 @@ docker run --name wdb-odb -p 0.0.0.0:27017:27017 -d mongo:latest
 alias mongo="docker run -it --link wdb-odb:mongo --rm mongo sh -c 'exec mongo \"\$MONGO_PORT_27017_TCP_ADDR:\$MONGO_PORT_27017_TCP_PORT\"'"
 
 # Web-interface
-docker run -d -P --name wdb-www --link wdb-odb:mongo whiplash/www:experimental
+docker run -p 80:80  -v /var/www/storage:/var/www/html/uploads -d -P --name wdb-www whiplash/www:experimental
 
 # Run-time
 docker run -d -P --name wdb-rte --link wdb-odb:mongo whiplash/rte-local:deploy sh -c "./drivers/scheduler.driver -dbhost \$MONGO_PORT_27017_TCP_ADDR:\$MONGO_PORT_27017_TCP_PORT"
