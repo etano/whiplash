ffb03d5ea8e189c9986cda72464fbcb97d6d837e
Ethan Brown <ethan.w.brown@gmail.com>
2015-10-06 14:19:48 +0200

    update www stuff to have splash page

--- www/html/index.php
+++ www/html/index.php
@@ -1,27 +1,15 @@
 <!DOCTYPE html>
 <html>
-<head> 
-    <title>Whiplash DB</title>
+<head>
+    <title>Whiplash</title>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
     <meta name="apple-mobile-web-app-capable" content="yes">
     <link rel="stylesheet" href="css/main.css" type="text/css" />
-    <script src="scripts/jquery/jquery-2.1.1.min.js"></script>
-    <script src="scripts/main.js"></script>
-</head> 
+</head>
 <body>
-    <section id="logo">Whiplash DB</section>
-    <section id="login">
-        <form id="login" method="post">
-            <input type="field" class="login-field" name="user" id="login-user" value="user" />
-            <input type="field" class="login-field" name="pass" id="login-pass" value="password" />
-            <input type="button" class="button" name="submit" id="login-submit" value="login" />
-        </form>
-    </section>
-    <section id="upload">
-        <form id="upload" method="post">
-            <input type="file" class="file" name="myfile" id="myfile-input" onchange="upload();" />
-            <label for="myfile-input">Upload dataset</label>
-        </form>
+    <section id="middle">
+        <section id="logo">Whiplash</section>
+        <section id="soon">coming soon...</section>
     </section>
 </body>
 </html>
--- www/html/upload/upload.php
+++ www/html/upload/upload.php
@@ -4,7 +4,7 @@ session_start();
 if($_SESSION["authorized"]!=TRUE){
     die("not authorized");
 }
-$target_dir = "uploads/";
+$target_dir = "../uploads/";
 $target_file = $target_dir . basename($_FILES["myfile"]["name"]);
 
 if ($_FILES["myfile"]["size"] > (99 * 1024 * 1024)) die("error");
