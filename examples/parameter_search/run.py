c51b6f931b0c331a244ee0a12c4bc1b667ecc565
Ilia Zintchenko <zintchenko@itp.phys.ethz.ch>
2015-10-06 14:28:46 +0200

    update parameter search + commented code

--- examples/parameter_search/run.py
+++ examples/parameter_search/run.py
@@ -1,41 +1,65 @@
-import subprocess,json,whiplashdb
-import math
+'''Parameter search using WhiplashDB. The search itself is done
+sequentially. However, each call to the cost function is very
+expensive. Here the cost is the expected complexity of finding the
+most optimal configuration for a model. This is computed by running
+simulated annealing for random initial states thousands of times. As
+the restarts are independent and takes approximately the same time to
+complete, they are efficiently fanned out with Whiplash and the
+results collected when at least a set fraction is competed. We use the
+MOE optimisation library developed by Yelp Research to find optimal
+parameters in as few queries to the cost function as possible.
 
+'''
+
+import subprocess,json,whiplashdb,math,random,sys
+
+#start whiplashdb client
 wdb = whiplashdb.wdb("localhost:27017")
 
+#commit model 
 model_id = wdb.CommitModel('src/tests/108ising.json')
+
+cost_opt = -165
+
+#commit executable
 executable_id = wdb.CommitExecutable({'class':'ising','description':'foo','algorithm':'SA','version':'bar','build':'O3','schedule':'linear','path':'./apps/spin_glass_solver/bin/main','name':'test'})
 
 n_reps = 1000 #number of repetitions
 p_chance = 0.99 #chance of finding optimal config
 
+#cost function for our search. here the expected number of sweeps to
+#obtain optimal configurationwith probability p_chance.
 def func(x):
 
+    #simulation parameters
     params = {'n_sweeps':x[0],'b0':x[1],'b1':x[2]}
-    status = 0
-    properties = [wdb.FormProperty('ising',model_id,executable_id,status,params) for i in range(n_reps)]
-    wdb.CommitProperties(properties)
 
-    property_filter = {'class':'ising','status':3}
-    
-    property_ids = []
-    while len(property_ids) < 0.99*n_reps:
-        property_ids = wdb.QueryProperties(property_filter)
+    #form property
+    properties = [wdb.FormProperty('ising',model_id,executable_id,0,random.random(),params) for i in range(n_reps)]
 
+    #commit properties and wait for at least 99% of them to be resolved
+    properties = wdb.ResolveProperties(properties,0.99)
+
+    #calculate number of times a configuration of the optimal cost was found
     num_success = 0
-    for id in property_ids:
-        property = wdb.FetchProperty(id)
+    for property in properties:
         if property['cfg']['costs'] == cost_opt:
             num_success += 1
 
+    #delete properties which are no longer needed
+    #wdb.DeleteProperties(properties)
+        
+    #success probability
     p_success = float(num_success) / len(property_ids)
 
+    #expected number of sweeps to obtain optimal configuration with probability p_success
     complexity = x[0]*math.log(1.0-p_chance)/math.log(1.0-p_success) #n_sweeps * expected number of repetitions
 
     return complexity
 
 data = {}
 
+#value ranges for where to search for parameters. here we have 3
 data['limits'] = []
 data['limits'].append([0.0,10.0]) #beta0
 data['limits'].append([0.0,10.0]) #beta1
@@ -45,20 +69,26 @@ data['var'] = 0.01 #noise
 
 data['points'] = []
 
+#first sample point
 x0 = [5.0,0.1,1000]
-
 data['points'].append([x0,func(x0)])
 
 json.dump(data, open('experiment.json','w'))
 
-for i in range(10):
+num_samples = 100
 
+#sample num_samples points, expecting each point to be a better guess
+#at the optimum that the others
+for i in range(num_samples):
+
+    #communicate with the MOE optimisation library through subprocess
     cmd = 'docker exec -it moe sh -c \"cd whiplashdb && python next_point.py\"'
-    x = json.loads(subprocess.check_output(cmd, shell=True))
 
+    x = json.loads(subprocess.check_output(cmd, shell=True))
     C = func(x)
 
     print i,x,C
 
+    #append new point to the data and write to file
     data['points'].append([x,C])
     json.dump(data, open('experiment.json','w'))
--- examples/parameter_search/startup.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-docker pull yelpmoe/latest
-docker run -d --name moe -v ${WDB_HOME}:/home/app/MOE/whiplashdb yelpmoe/latest
