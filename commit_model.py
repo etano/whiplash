43fa43b71df0f5f576ab3a65d7475bfa3f094009
Ethan Brown <ethan.w.brown@gmail.com>
2015-10-19 16:24:20 +0200

    major overhaul to add necessary api calls and unify things

--- commit_model.py
+++ commit_model.py
@@ -1,9 +1,9 @@
 import whiplash,json
 
 # Connect to local Whiplash instance
-wdb = whiplash.wdb("whiplash.ethz.ch","1337","93690945c6e6ad77ab47b15f77c4831942bc5ab24ea2662754d02967d2a23f29")
+wdb = whiplash.wdb("whiplash.ethz.ch","1337","e0affd5b02be43f644ceea8ad8f7f8e2fa48afa9dfab06fc3f57190740c16ee9")
 model = json.load(open('108ising.json'))
 models = []
-for i in range(100):
+for i in range(1):
     models.append(model)
-print wdb.CommitModels(models)
+print wdb.models.commit(models)
--- whiplash.py
+++ whiplash.py
@@ -1,36 +1,58 @@
 import sys, json
 if sys.version_info[0] < 3: import httplib
-else: import http.client as httplib    
+else: import http.client as httplib
 
+#
 # Whiplash class
+#
 class wdb:
     def __init__(self,server,port,access_token=""):
         self.server = server
         self.port = port
-        self.models = "models"
-        self.executables = "executables"
-        self.properties = "properties"
+        self.set_token(access_token)
+        self.check_token()
 
-        self.SetToken(access_token)
-        self.CheckToken()
+        self.models = self.collection(self,"models")
+        self.executables = self.collection(self,"executables")
+        self.properties = self.properties_collection(self,"properties")
 
-    def CheckToken(self):
-        status, reason, res = self.Request("GET","/api",json.dumps({"foo":"bar"}))
+    #
+    # Request
+    #
+
+    def request(self,protocol,uri,payload):
+        conn = httplib.HTTPConnection(self.server,self.port)
+        conn.request(protocol,uri,payload,self.headers)
+        res = conn.getresponse()
+        if res.status != 200:
+            print(res.status, res.reason)
+        return res.status, res.reason, res.read()
+
+    #
+    # Tokens
+    #
+
+    def set_token(self,access_token):
+        self.access_token = access_token
+        self.headers = {"Content-type": "application/json", "Accept": "*/*", "Authorization":"Bearer "+self.access_token}
+
+    def check_token(self):
+        status, reason, res = self.request("GET","/api",json.dumps({"foo":"bar"}))
         if status != 200:
             if 'Unauthorized' in reason:
                 print('Token not valid. Please create one.')
-                self.CreateToken()
+                self.create_token()
             else:
                 sys.exit()
 
-    def CreateToken(self):
+    def create_token(self):
         username = raw_input("username: ")
         password = raw_input("password: ")
         client_id = raw_input("client ID: ")
         client_secret = raw_input("client secret: ")
 
         self.headers = {"Content-type": "application/json", "Accept": "*/*"}
-        status, reason, res = self.Request("POST","/api/oauth/token",json.dumps({"grant_type":"password","client_id":client_id,"client_secret":client_secret,"username":username,"password":password}))
+        status, reason, res = self.request("POST","/api/oauth/token",json.dumps({"grant_type":"password","client_id":client_id,"client_secret":client_secret,"username":username,"password":password}))
         if status != 200:
             if ('Unauthorized' in reason) or ('Forbidden' in reason):
                 print('Invalid login credentials. Please go to http://whiplash.ethz.ch to verify your account.')
@@ -40,147 +62,110 @@ class wdb:
             print("New tokens grant for", res["expires_in"], "seconds. Please save them.")
             print("access token:", res["access_token"])
             print("refresh token:", res["refresh_token"])
-            self.SetToken(res["access_token"])
+            self.set_token(res["access_token"])
 
-    def Request(self,protocol,uri,payload):
-        conn = httplib.HTTPConnection(self.server,self.port)
-        conn.request(protocol,uri,payload,self.headers)
-        res = conn.getresponse()
-        if res.status != 200:
-            print(res.status, res.reason)
-        return res.status, res.reason, res.read()
-
-    def SetToken(self,access_token):
-        self.access_token = access_token
-        self.headers = {"Content-type": "application/json", "Accept": "*/*", "Authorization":"Bearer "+self.access_token}
-
-    def RefreshToken(self,refresh_token):
+    def refresh_token(self,refresh_token):
         client_id = raw_input("client ID: ")
         client_secret = raw_input("client secret: ")
 
         self.headers = {"Content-type": "application/json", "Accept": "*/*"}
-        status, reason, res = self.Request("POST","/api/oauth/token",json.dumps({"grant_type":"refresh_token","client_id":client_id,"client_secret":client_secret,"refresh_token":refresh_token}))
+        status, reason, res = self.request("POST","/api/oauth/token",json.dumps({"grant_type":"refresh_token","client_id":client_id,"client_secret":client_secret,"refresh_token":refresh_token}))
         if status != 200:
             if ('Unauthorized' in reason) or ('Forbidden' in reason):
                 print('Invalid refresh token. Please login to get new tokens.')
-                self.CreateToken()
+                self.create_token()
             sys.exit()
         else:
             res = json.loads(res.decode('utf-8'))
             print("New tokens grant for", res["expires_in"], "seconds. Please save them.")
             print("access token:", res["access_token"])
             print("refresh token:", res["refresh_token"])
-            self.SetToken(res["access_token"])
+            self.set_token(res["access_token"])
 
-    def InsertMany(self,collection,objects):
-        status, reason, res = self.Request("POST","/api/"+collection,json.dumps(objects))
-        return json.loads(res.decode('utf-8'))["ids"]
+    #
+    # Collections
+    #
+    class collection:
+        def __init__(self,db,name):
+            self.name = name
+            self.db = db
 
-    def InsertOne(self,collection,object):
-        return self.InsertMany(collection,[object])[0]
+        #
+        # Commit
+        #
 
-    def DeleteMany(self,collection,objects):
-        status, reason, res = self.Request("DELETE","/api/"+collection,json.dumps(objects))
-        return json.loads(res.decode('utf-8'))
+        def commit(self,objects):
+            if not isinstance(objects, list):
+                objects = [objects]
+            status, reason, res = self.db.request("POST","/api/"+self.name+"/commit/",json.dumps(objects))
+            return json.loads(res.decode('utf-8'))["ids"]
 
-    def Find(self,collection,filter):
-        status, reason, res = self.Request("GET","/api/"+collection,json.dumps(filter))
-        return json.loads(res.decode('utf-8'))["objs"]
+        #
+        # Query
+        #
 
-    def FindById(self,collection,id):
-        status, reason, res = self.Request("GET","/api/"+collection+"/"+str(id),{})
-        return json.loads(res.decode('utf-8'))["obj"]
+        def query(self,filter):
+            status, reason, res = self.db.request("GET","/api/"+self.name+"/query/",json.dumps(filter))
+            return json.loads(res.decode('utf-8'))["objs"]
 
-    def FindByIdAndUpdate(self,collection,id,object):
-        status, reason, res = self.Request("PUT","/api/"+collection+"/FindByIdAndUpdate/"+str(id),json.dumps(object))
-        return json.loads(res.decode('utf-8'))["obj"]
+        def query_by_id(self,id):
+            status, reason, res = self.db.request("GET","/api/"+self.name+"/query/"+str(id),{})
+            return json.loads(res.decode('utf-8'))["obj"]
 
-    def FindOneAndUpdate(self,collection,object):
-        status, reason, res = self.Request("PUT","/api/"+collection+"/FindOneAndUpdate",json.dumps(object))
-        return json.loads(res.decode('utf-8'))["obj"]
+        def query_for_ids(self,filter):
+            ids = []
+            for res in self.query(filter):
+                ids.append(res['_id'])
+            return ids
 
-    def UpdateById(self,collection,id,object):
-        status, reason, res = self.Request("PUT","/api/"+collection+"/Update/"+str(id),json.dumps(object))
-        return json.loads(res.decode('utf-8'))
+        #
+        # Update
+        #
 
-    def FormJson(self,collection,object):
-        if type(object) is str:
-            object = json.load(open(object))
-        return object
+        def find_one_and_update(self,filter,update):
+            status, reason, res = self.db.request("PUT","/api/"+self.name+"/update/",json.dumps({'filter':filter,'update':object}))
+            return json.loads(res.decode('utf-8'))["obj"]
 
-    def Commit(self,collection,object):
-        object = self.FormJson(collection,object)
-        return self.InsertOne(collection,object)
+        def update_by_id(self,id,object):
+            status, reason, res = self.db.request("PUT","/api/"+self.name+"/update/"+str(id),json.dumps(object))
+            return json.loads(res.decode('utf-8'))["obj"]
 
-    def CommitMany(self,collection,objects):
-        for object in objects:
-            object = self.FormJson(collection,object)
-        return self.InsertMany(collection,objects)
+        #
+        # Delete
+        #
 
-    def CommitModel(self,object):
-        return self.Commit(self.models,object)
+        def delete(self,filter):
+            status, reason, res = self.db.request("DELETE","/api/"+self.name+"/delete/",json.dumps(filter))
+            return json.loads(res.decode('utf-8'))
 
-    def CommitModels(self,objects):
-        return self.CommitMany(self.models,objects)
+        def delete_by_id(self,id):
+            status, reason, res = self.db.request("DELETE","/api/"+self.name+"/delete/"+str(id),{})
+            return json.loads(res.decode('utf-8'))
 
-    def CommitExecutable(self,object):
-        return self.Commit(self.executables,object)
+        def delete_by_ids(self,ids):
+            return self.delete({'_id': { '$in': ids }})
 
-    def CommitExecutables(self,objects):
-        return self.CommitMany(self.executables,objects)
+    #
+    # Special helper functions, only for properties
+    #
+    class properties_collection(collection):
 
-    def CommitProperty(self,object):
-        return self.Commit(self.properties,object)
+        def hold_until_resolved(self,property_ids,fraction):
+            #TODO: if more efficient, fetch by unique tag for a given set
+            #of jobs rather than array of ids
+            while True:
+                properties = self.query(self.properties,{'_id': { '$in': property_ids },'status':3})
+                if properties.count() > fraction*len(property_ids): break
+            return properties
 
-    def CommitProperties(self,objects):
-        return self.CommitMany(self.properties,objects)
+        def form(self,class_name,model_id,executable_id,status,seed,params):
+            return {'class':class_name,'executable_id':executable_id,'model_id':model_id,'status':status,'seed':seed,'params':params}
 
-    def HoldUntilResolved(self,property_ids,fraction):
-        #TODO: if more efficient, fetch by unique tag for a given set
-        #of jobs rather than array of ids
-        while True:
-            properties = self.Find(self.properties,{'_id': { '$in': property_ids },'status':3})
-            if properties.count() > fraction*len(property_ids): break
-        return properties
+        def get_unresolved(self):
+            return self.find_one_and_update({'status':0},{'status':1})
 
-    def Query(self,collection,filter):
-        ids = []
-        for res in self.Find(collection,filter):
-            ids.append(res['_id'])
-        return ids
+        def commit_resolved(self,object):
+            return self.update_by_id(object["_id"],object)
 
-    def QueryModels(self,filter):
-        return self.Query(self.models,filter)
-
-    def QueryExecutables(self,filter):
-        return self.Query(self.executables,filter)
-
-    def QueryProperties(self,filter):
-        return self.Query(self.properties,filter)
-
-    def Fetch(self,collection,id):
-        return self.FindById(collection,id)
-
-    def FetchModel(self,id):
-        return self.Fetch(self.models,id)
-
-    def FetchExecutable(self,id):
-        return self.Fetch(self.executables,id)
-
-    def FetchProperty(self,id):
-        return self.Fetch(self.properties,id)
-
-    def DeleteProperties(self,ids):
-        return self.DeleteMany(self.properties,{'_id': { '$in': ids }})
-
-    def FormProperty(self,class_name,model_id,executable_id,status,seed,params):
-        return {'class':class_name,'executable_id':executable_id,'model_id':model_id,'status':status,'seed':seed,'params':params}
-
-    def GetUnresolvedProperty(self):
-        return self.FindOneAndUpdate(self.properties,{'filter':{'status':0},'update':{'status':1}})
-
-    def CommitResolvedProperty(self,object):
-        return self.UpdateById(self.properties,object["_id"],object)
-
-    def UpdatePropertyStatus(self,id,status):
-        return self.UpdateById(self.properties,id,{'status':status})
+        def update_status(self,id,status):
+            return self.update_by_id(id,{'status':status})
