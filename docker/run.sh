f7d929dd7aed4b30aaa1090485d2f0ae0bd5ade2
Ethan Brown <ethan.w.brown@gmail.com>
2015-10-19 23:33:26 +0200

    updated run.sh

--- docker/run.sh
+++ docker/run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run --name wdb-api -p 1337:1337 -e "MONGO_URI=mongodb://ds053190.mongolab.com:53190/wdb" -e "MONGO_READWRITEUSER_USERNAME=readWriteUser" -e "MONGO_READWRITEUSER_PASSWORD=haYrv{Ak9UJiaDsqVTe7rLJTc" -d -t whiplash/api sh -c "cd src; node bin/www"
+docker run --name wdb-api -p 1337:1337 -e "MONGO_URI=mongodb://whiplash.ethz.ch:27017/wdb" -e "MONGO_READWRITEUSER_USERNAME=readWriteUser" -e "MONGO_READWRITEUSER_PASSWORD=haYrv{Ak9UJiaDsqVTe7rLJTc" -d -t whiplash/api sh -c "cd src; node bin/www"
