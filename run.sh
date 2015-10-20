323ca94a63e83c4de9488dc8e2420b9114794e5a
Ilia Zintchenko <zintchenko@itp.phys.ethz.ch>
2015-10-20 22:43:25 +0200

    only master communicates with database

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 
 wdb_info=$1
 job_number=$2
-cpu_time=$3
+time_limit=$3
 
 ###
 
@@ -19,10 +19,10 @@ echo "#SBATCH --job-name=${job_name}" >> $run_file
 echo "#SBATCH --output=${dir_log}/out.o" >> $run_file
 echo "#SBATCH --error=${dir_log}/out.e" >> $run_file
 echo "#SBATCH --partition=dphys_compute" >> $run_file
-echo "#SBATCH --time=${cpu_time}:00:00" >> $run_file
+echo "#SBATCH --time=${time_limit}:00:00" >> $run_file
 echo "#SBATCH --nodes=1" >> $run_file
 echo "#SBATCH --exclusive" >> $run_file
 echo "#SBATCH --ntasks=1" >> $run_file
-echo "srun python scheduler_local.py --wdb_info ${wdb_info}" >> $run_file
+echo "srun python scheduler_local.py --wdb_info ${wdb_info} --time_limit ${time_limit}" >> $run_file
 
 sbatch $run_file
--- scheduler_local.py
+++ scheduler_local.py
@@ -1,47 +1,29 @@
 #!/usr/bin/env python3.4
 
-#TODO: batch request for unresolved properties by scheduler from
-#DB. when all properties in queue are resolved, scheduler empties the
-#completed queue and requests new unresolved properties.
-
-#TODO: request work according to how much the node can handle in the
-#time limit of the scheduler. work_time = num_cpus *
-#tasks_time. dispatch work according to time limits by adding the
-#largest job which fits until no more space
-
-#TODO: time limit on scheduler. close to time limit scheduler does not
-#request any more from DB and cleans up completed queue.
-
-#TODO: regular refresh of properties which never finished
-
-#TODO: each interval scheduler commits completed work to DB
-
 import multiprocessing as mp
 import subprocess as sp
 import whiplash,time,json,os,argparse,daemon,sys
 
-def resolve_property(wdb,pid,unresolved):
+def resolve_property(pid,unresolved,resolved):
     print('worker',str(pid),'active')
     while True:
-        prop = unresolved.get()
+        obj = unresolved.get()
 
-        executable = wdb.executables.query_by_id(prop['executable_id'])
-        model = wdb.models.query_by_id(prop['model_id'])
+        prop = obj['property']
+        ID = prop['_id']
 
-        print('worker',str(pid),'staring property:',prop['_id'])
+        print('worker',str(pid),'staring property',ID)
 
-        file_name = 'property_' + str(pid) + '_' + str(prop['_id']) + '.json'
+        file_name = 'property_' + str(pid) + '_' + str(ID) + '.json'
 
-        package = {'model':model,'params':prop['params']}
+        package = {'model':obj['model'],'params':prop['params']}
         with open(file_name, 'w') as propfile:
             json.dump(package, propfile)
 
-        path = executable['path']
+        path = obj['executable']['path']
         timeout = prop['timeout']
 
         try:
-            wdb.properties.update_status(prop['_id'],2)
-
             t0 = time.time()
             sp.call(path + [file_name],timeout=timeout)
             t1 = time.time()
@@ -54,13 +36,12 @@ def resolve_property(wdb,pid,unresolved):
                 prop['result'] = json.load(propfile)
 
             prop['status'] = 4
-
-            print('worker',str(pid),'commiting property:',prop['_id'],'walltime:',elapsed)
-            wdb.properties.commit_resolved(prop)
-
+            print('worker',str(pid),'resolved property',ID,'with walltime',elapsed)
         except sp.TimeoutExpired:
-            wdb.properties.update_status(prop['_id'],3)
+            prop['status'] = 3
             print('time expired for property',prop['_id'],'on worker',str(pid))
+
+        resolved.put(prop)
 
         os.remove(file_name)
 
@@ -79,28 +60,42 @@ def run(args):
 
     context = mp.get_context('fork')
     unresolved = context.Queue()
+    resolved = context.Queue()
 
     print('starting workers')
     for pid in range(num_cpus-1):
-        p = context.Process(target=resolve_property, args=(wdb,pid,unresolved,))
+        p = context.Process(target=resolve_property, args=(pid,unresolved,resolved,))
         p.start()
 
     print('starting queries')
     while True:
-        prop = wdb.properties.get_unresolved()
-        if prop != {}:
-            print('unresolved property:',prop['_id'])
-            unresolved.put(prop)
+
+        if not resolved.empty():
+            prop = resolved.get()
+            if prop['status'] == 3:
+                wdb.properties.update_status(prop['_id'],3)
+            else:
+                wdb.properties.commit_resolved(prop)
         else:
-            print('all properties are currently resolved')
-            if args.exit_on_resolved: sys.exit(0)
-        time.sleep(0.1)
+            prop = wdb.properties.get_unresolved()
+            if prop != {}:
+                print('unresolved property',prop['_id'])
+                model = wdb.models.query_by_id(prop['model_id'])
+                executable = wdb.executables.query_by_id(prop['executable_id'])
+                obj = {'property':prop,'model':model,'executable':executable}
+                unresolved.put(obj)
+            else:
+                print('all properties are currently resolved')
+                if args.exit_on_resolved: sys.exit(0)
+            time.sleep(0.1)
 
 if __name__ == '__main__':
 
+    #TODO: batch get and commit requests to DB depending on time_limit
+
     parser = argparse.ArgumentParser()
     parser.add_argument('--wdb_info',dest='wdb_info',required=True,type=str)
-    parser.add_argument('--time_limit',dest='time_limit',required=True,type=int)    
+    parser.add_argument('--time_limit',dest='time_limit',required=True,type=int)
     parser.add_argument('--num_cpus',dest='num_cpus',required=False,type=int)
     parser.add_argument('--log_file',dest='log_file',required=False,type=str,default='scheduler_local_' + str(int(time.time())) + '.log')
     parser.add_argument('--exit_on_resolved',dest='exit_on_resolved',required=False,default=False,action='store_true')
