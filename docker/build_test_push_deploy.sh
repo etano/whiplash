bd20d44edbb58fb4d579e2f1e7426500fe254abc
Ethan Brown <ethan.w.brown@gmail.com>
2015-10-22 00:08:43 +0200

    got it

--- docker/build_test_push_deploy.sh
+++ docker/build_test_push_deploy.sh
@@ -3,11 +3,18 @@
 # testing flags
 set -e
 trap 'previous_command=$this_command; this_command=$BASH_COMMAND' DEBUG
-trap './cleanup.sh; echo "exit $? : $previous_command"' EXIT
+trap './cleanup.sh; mail -s "exit $? : $previous_command" "ebrown@itp.phys.ethz.ch"' EXIT
 
-# build containers
+#
+# BUILD
+#
+
 docker build -t whiplash/odb -f Dockerfile.odb .
 docker build -t whiplash/api -f Dockerfile.api ..
+
+#
+# TEST
+#
 
 # run containers
 docker run --name wdb-odb-test -d whiplash/odb --auth
@@ -19,9 +26,24 @@ docker run -it --link wdb-odb-test:mongo -e "MONGO_API_USERNAME=api" -e "MONGO_A
 # create client
 docker run -it --link wdb-odb-test:mongo -e "MONGO_API_USERNAME=api" -e "MONGO_API_PASSWORD=haYrv{Ak9UJiaDsqVTe7rLJTc" -t whiplash/api sh -c "cd src; MONGO_URI=mongodb://\$MONGO_PORT_27017_TCP_ADDR:\$MONGO_PORT_27017_TCP_PORT/wdb node bin/createClient.js browser 32489 ha87hjlAWidwrxv435est"
 
-# push containers
+# cleanup
+./cleanup.sh
+
+#
+# PUSH
+#
+
 docker push whiplash/odb
 docker push whiplash/api
+
+#
+# DEPLOY
+#
+
+# turn off testing
+set +e
+trap - DEBUG
+trap - EXIT
 
 # deploy
 ./deploy.sh
--- docker/cleanup.sh
+++ docker/cleanup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-# kill and remove containers
+# kill and remove test containers
 docker stop wdb-api-test; docker rm wdb-api-test;
 docker stop wdb-odb-test; docker rm wdb-odb-test;
