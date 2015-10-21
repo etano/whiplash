047e49b8b7002bb3c0ab87d0400808f8ee57b810
Ethan Brown <ethan.w.brown@gmail.com>
2015-10-21 23:26:59 +0200

    added cleanup.sh

--- docker/build_test_push.sh
+++ docker/build_test_push.sh
@@ -3,19 +3,15 @@
 # testing flags
 set -e
 trap 'previous_command=$this_command; this_command=$BASH_COMMAND' DEBUG
-trap 'echo "exit $? : $previous_command"' EXIT
+trap './cleanup.sh; echo "exit $? : $previous_command"' EXIT
 
-# build wdb-odb
+# build containers
 docker build -t whiplash/odb -f Dockerfile.odb .
-
-# start wdb-odb-test
-docker run --name wdb-odb-test -d whiplash/odb --auth
-
-# build wdb-api
 docker build -t whiplash/api -f Dockerfile.api ..
 
-# start wdb-api-test
-docker run --link wdb-odb-test:mongo --name wdb-api-test -p 1338:1337 -d -P -e "MONGO_API_USERNAME=api" -e "MONGO_API_PASSWORD=haYrv{Ak9UJiaDsqVTe7rLJTc" -t whiplash/api sh -c "cd src; MONGO_URI=mongodb://\$MONGO_PORT_27017_TCP_ADDR:\$MONGO_PORT_27017_TCP_PORT/wdb node bin/www"
+# run containers
+docker run --name wdb-odb-test -d whiplash/odb --auth
+docker run --name wdb-api-test --link wdb-odb-test:mongo -p 1338:1337 -d -P -e "MONGO_API_USERNAME=api" -e "MONGO_API_PASSWORD=haYrv{Ak9UJiaDsqVTe7rLJTc" -t whiplash/api sh -c "cd src; MONGO_URI=mongodb://\$MONGO_PORT_27017_TCP_ADDR:\$MONGO_PORT_27017_TCP_PORT/wdb node bin/www"
 
 # create user
 docker run -it --link wdb-odb-test:mongo -e "MONGO_API_USERNAME=api" -e "MONGO_API_PASSWORD=haYrv{Ak9UJiaDsqVTe7rLJTc" -t whiplash/api sh -c "cd src; MONGO_URI=mongodb://\$MONGO_PORT_27017_TCP_ADDR:\$MONGO_PORT_27017_TCP_PORT/wdb node bin/createUser.js www 7cJgeAkHdw{oktPNYdgYE3nJ"
@@ -26,7 +22,3 @@ docker run -it --link wdb-odb-test:mongo -e "MONGO_API_USERNAME=api" -e "MONGO_A
 # push containers
 docker push whiplash/odb
 docker push whiplash/api
-
-# kill and remove containers
-docker stop wdb-api-test; docker rm wdb-api-test;
-docker stop wdb-odb-test; docker rm wdb-odb-test;
