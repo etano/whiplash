4f739f9fbb7349967a907fbcdb80354e7a37bbd7
Ilia Zintchenko <zintchenko@itp.phys.ethz.ch>
2015-10-23 16:38:52 +0200

    fix model and executable objectid

--- libs/schemas/property.js
+++ libs/schemas/property.js
@@ -4,8 +4,10 @@ var Schema = mongoose.Schema;
 // Property
 var Property = new Schema({
     owner: { type: String, required: true },
-    model_id: { type: Number, required: true},
-    executable_id: { type: Number, required: true},
+    //model_id: { type: Number, required: true},
+    model_id: { type: Schema.Types.ObjectId, required: true},
+    //executable_id: { type: Number, required: true},
+    executable_id: { type: Schema.Types.ObjectId, required: true},
     status: { type: String, required: true, default: "unresolved"},
     result: { type: Schema.Types.Mixed, default: "" },
     params: { type: Schema.Types.Mixed, default: "" },
