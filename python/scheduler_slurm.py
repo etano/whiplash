6b80935a8b0b7dd9345ef572f618073c6c150a6f
Ilia Zintchenko <zintchenko@itp.phys.ethz.ch>
2015-11-03 16:56:12 +0100

    slurm scheduler prepares monch

--- python/scheduler_slurm.py
+++ python/scheduler_slurm.py
@@ -13,13 +13,20 @@ def run(args):
 
     job_number = 0
 
+    sp.call("ssh " + args.cluster + " \"bash -lc \'" + "mkdir -p rte" + "\'\"",shell=True)
+    for FILE in ["whiplash.py","run.sh","scheduler_local.py",args.wdb_info]:
+        sp.call("scp " + FILE + " " + args.cluster + ":rte/",shell=True)
+
+    queue_command = "squeue -u whiplash | grep \"PD\" | wc -l"
+    scheduler_command = "cd rte && sh run.sh"
+
     while True:
         num_unresolved = wdb.properties.get_num_unresolved()
-        num_pending = int(sp.check_output("ssh " + args.cluster + " \'squeue -u whiplash | grep \"PD\" | wc -l\'", shell=True))
+        num_pending = int(sp.check_output("ssh " + args.cluster + " \'" + queue_command + "\'", shell=True))
         print('unresolved:',num_unresolved,' | ','pending:',num_pending)
         if num_unresolved > 0 and num_pending == 0:
             print('submitting job')
-            sp.call("ssh " + args.cluster + " \"bash -lc \'cd /users/whiplash/whiplash/whiplash/python && sh run.sh " + args.wdb_info + " " + str(job_number) + " " + str(args.time_limit) + " " + str(args.job_limit) + " " + str(args.time_window) + " " + str(args.num_cpus) + "\'\"",shell=True)
+            sp.call("ssh " + args.cluster + " \"bash -lc \'" + scheduler_command + " " + args.wdb_info + " " + str(job_number) + " " + str(args.time_limit) + " " + str(args.job_limit) + " " + str(args.time_window) + " " + str(args.num_cpus) + "\'\"",shell=True)
             job_number += 1
         time.sleep(5)
     
@@ -30,7 +37,7 @@ if __name__ == '__main__':
     parser.add_argument('--time_limit',dest='time_limit',required=True,type=int)
     parser.add_argument('--job_limit',dest='job_limit',required=False,type=int,default=1000)
     parser.add_argument('--time_window',dest='time_window',required=True,type=int)
-    parser.add_argument('--num_cpus',dest='num_cpus',required=False,type=int)
+    parser.add_argument('--num_cpus',dest='num_cpus',required=False,type=int,default=20)
     parser.add_argument('--cluster',dest='cluster',required=True,type=str)
     parser.add_argument('--log_file',dest='log_file',required=False,type=str,default='scheduler_slurm_' + str(int(time.time())) + '.log')
     parser.add_argument('--daemonise',dest='daemonise',required=False,default=False,action='store_true')
