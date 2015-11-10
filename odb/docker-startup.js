b4ddb34160c1ce03a47c3e338c9c5514e55e987a
Ilia Zintchenko <zintchenko@itp.phys.ethz.ch>
2015-11-10 11:31:13 +0100

    more reasonable password for dbowner

--- odb/docker-startup.js
+++ odb/docker-startup.js
@@ -1,6 +1,6 @@
 db.createUser({user:"www",pwd:"7cJgeAkHdw{oktPNYdgYE3nJ",roles:[{role:"readWrite",db:"wdb"}]});
 db.createUser({user:"api",pwd:"haYrv{Ak9UJiaDsqVTe7rLJTc",roles:[{role:"readWrite",db:"wdb"}]});
-db.createUser({user:"pwn",pwd:"cftXzdrjheHEAR!JKT(>>,V",roles:[{role:"dbOwner",db:"wdb"}]});
+db.createUser({user:"pwn",pwd:"cftXzdrjheHEARuJKT39x]3sV",roles:[{role:"dbOwner",db:"wdb"}]});
 db.fs.files.createIndex({md5 : 1, "metadata.property_id" : 1},{unique : true});
 db.executables.createIndex({name : 1, algorithm : 1, version : 1, build : 1},{unique : true});
 db.properties.createIndex({input_model_id : 1, executable_id : 1, params : 1},{unique : true});
