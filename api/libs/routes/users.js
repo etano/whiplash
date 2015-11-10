428a5b09af577bba9d5f32f79af26bef21808d01
Ilia Zintchenko <zintchenko@itp.phys.ethz.ch>
2015-11-10 20:15:02 +0100

    users scheduler uses pymongo

--- api/libs/routes/users.js
+++ api/libs/routes/users.js
@@ -7,10 +7,6 @@ var log = require(libs + 'log')(module);
 var config = require(libs + 'config');
 var User = require(libs + 'schemas/user');
 
-var common = require(libs + 'routes/common');
-var db = require(libs + 'db/mongo');
-var collection = db.get().collection('users');
-
 var webAuth = function(req, res, next){
     var token = req.body.server_token;
     if(token === config.get('WebAccessToken')) {
@@ -31,18 +27,6 @@ router.post('/', webAuth, function(req, res){
             res.send("Bof");
         }
     });
-});
-
-router.get('/', passport.authenticate('bearer', { session: false }), function(req, res) {
-    // TODO: check for authorized user
-    var filter = req.body;
-    common.query(collection,filter,res);
-});
-
-router.get('/tokens/', passport.authenticate('bearer', { session: false }), function(req, res) {
-    // TODO: check for authorized user
-    var filter = req.body;
-    common.query(db.get().collection('accesstokens'),filter,res);
 });
 
 module.exports = router;
--- python/scheduler_users.py
+++ python/scheduler_users.py
@@ -1,27 +1,19 @@
 #!/usr/bin/env python3
 
-import whiplash,daemon,argparse,time,sys,os,json
+import daemon,argparse,time,sys,os,json,pymongo
 import subprocess as sp
 import threading as th
 
 def start_scheduler_slurm(wdb_user):
     sp.call("./scheduler_slurm.py" + " --user " + wdb_user['username'] + " --token " + wdb_user['token'] + " --cluster " + wdb_user['cluster'],shell=True)
 
-def get_request(wdb,uri):
-    status, reason, res = wdb.request("GET",uri,json.dumps({}))
-    if status == 200:
-        return json.loads(res.decode('utf-8'))["result"]
-    else:
-        return []
-        print(status,reason,res)
-    
 def get_users(wdb):
-    users = get_request(wdb,"/api/users/")
-    tokens = get_request(wdb,"/api/users/tokens")
+    users = wdb.users.find({})
+    tokens = wdb.accesstokens.find({})
     wdb_users = []
     for user in users:
         for token in tokens:
-            if user['_id'] == token['userId']:
+            if str(user['_id']) == token['userId']:
                 cluster = "monch.cscs.ch" #TODO: dedicated collection of clusters
                 wdb_users.append({'username':user['username'],'token':token['token'],'cluster':cluster})
                 break
@@ -29,28 +21,25 @@ def get_users(wdb):
 
 def scheduler(args):
 
-    wdb = whiplash.wdb(args.host,args.port,args.token)
+    client = pymongo.MongoClient(args.host,27017)
+    client.wdb.authenticate('scheduler','c93lbcp0hc[5209sebf10{3ja',mechanism='SCRAM-SHA-1')
 
     print('scheduler connected to wdb')
 
     users = []
     while True:
-
-        for wdb_user in get_users(wdb):
+        for wdb_user in get_users(client.wdb):
             user = wdb_user['username']
             if user not in users:
                 print('starting slurm scheduler for user',user)
                 th.Thread(target = start_scheduler_slurm, args = (wdb_user,)).start()
                 users.append(user)
-
         time.sleep(60)
 
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser()
     parser.add_argument('--host',dest='host',required=False,type=str,default="whiplash.ethz.ch")
-    parser.add_argument('--port',dest='port',required=False,type=int,default=443)
-    parser.add_argument('--token',dest='token',required=True,type=str)
     parser.add_argument('--log_file',dest='log_file',required=False,type=str,default='scheduler_users_' + str(int(time.time())) + '.log')
     parser.add_argument('--daemonise',dest='daemonise',required=False,default=False,action='store_true')
     args = parser.parse_args()
