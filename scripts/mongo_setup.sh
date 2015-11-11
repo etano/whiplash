e689e694f935479fe85e893a24017126986b9f6d
Ilia Zintchenko <zintchenko@itp.phys.ethz.ch>
2015-11-11 18:38:21 +0100

    fix mongo setup

--- scripts/mongo_setup.sh
+++ scripts/mongo_setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-mongod --bind_ip=127.0.0.1 --dbpath /mnt/lnec/whiplash/data/db
+mongod --bind_ip=127.0.0.1 --dbpath /mnt/lnec/whiplash/data/db --logpath /users/whiplash/logs/mongod.log --fork
 sleep 5
-mongo 127.0.0.1:27017/wdb odb/docker-startup.sh
+mongo 127.0.0.1:27017/wdb odb/docker-startup.js
 sleep 5
 mongod --shutdown
