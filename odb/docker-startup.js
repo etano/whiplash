462652ef35f52e7acd9f752c18d21b07bc786c4c
Ilia Zintchenko <zintchenko@itp.phys.ethz.ch>
2015-11-23 14:52:28 +0100

    added owner to indexes

--- odb/docker-startup.js
+++ odb/docker-startup.js
@@ -2,7 +2,7 @@ db.createUser({user:"www",pwd:"7cJgeAkHdw{oktPNYdgYE3nJ",roles:[{role:"readWrite
 db.createUser({user:"api",pwd:"haYrv{Ak9UJiaDsqVTe7rLJTc",roles:[{role:"readWrite",db:"wdb"}]});
 db.createUser({user:"pwn",pwd:"cftXzdrjheHEARuJKT39x]3sV",roles:[{role:"dbOwner",db:"wdb"}]});
 db.createUser({user:"scheduler",pwd:"c93lbcp0hc[5209sebf10{3ca",roles:[{role:"read",db:"wdb"}]});
-db.fs.files.createIndex({md5 : 1, "metadata.property_id" : 1},{unique : true});
-db.executables.createIndex({name : 1, algorithm : 1, version : 1, build : 1},{unique : true});
-db.properties.createIndex({input_model_id : 1, executable_id : 1, params : 1},{unique : true});
+db.fs.files.createIndex({md5 : 1, "metadata.property_id" : 1, "metadata.owner" : 1},{unique : true});
+db.executables.createIndex({name : 1, algorithm : 1, version : 1, build : 1, owner : 1},{unique : true});
+db.properties.createIndex({input_model_id : 1, executable_id : 1, params : 1, owner : 1},{unique : true});
 db.setProfilingLevel(2)
