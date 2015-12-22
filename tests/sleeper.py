45de1ea610c986621253d240e07c17db953ef5db
Ethan Brown <ethan.w.brown@gmail.com>
2015-12-22 02:22:18 +0100

    fix sleeper.py, add executable params in test

--- tests/sleeper.py
+++ tests/sleeper.py
@@ -4,10 +4,10 @@ import json,sys,time
 
 file_name = sys.argv[1]
 
-time.sleep(2.0)
-
 with open(file_name, 'r') as infile:
     data = json.load(infile)
+
+time.sleep(data["params"]["sleep_time"])
 
 with open(file_name, 'w') as outfile:
     json.dump({"number":8}, outfile)
