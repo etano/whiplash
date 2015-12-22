c5d2c5bc31ef34e15645c8db8c8979a46428fa41
Ethan Brown <ethan.w.brown@gmail.com>
2015-12-22 15:53:45 +0100

    still tweaking

--- scripts/restart_all_monch.sh
+++ scripts/restart_all_monch.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
+echo 'Killing python'
 killall python3
+echo 'Killing node'
 killall node
-killall mongod
 
+echo 'Preparing RTE'
 bash ~/whiplash/scripts/prepare_rte.sh
-bash ~/whiplash/scripts/mongo_startup.sh
+echo 'Starting node'
 bash ~/whiplash/scripts/node_startup.sh
+echo 'Starting python'
 bash ~/whiplash/scripts/scheduler_startup.sh
