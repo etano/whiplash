b88ba7e6e2204c156da9820f30f3ba3c4519fe14
Ethan Brown <ethan.w.brown@gmail.com>
2015-12-22 02:21:09 +0100

    enforce params specification for executables

--- odb/startup.js
+++ odb/startup.js
@@ -3,7 +3,7 @@ db.createUser({user:"api",pwd:"haYrv{Ak9UJiaDsqVTe7rLJTc",roles:[{role:"readWrit
 db.createUser({user:"pwn",pwd:"cftXzdrjheHEARuJKT39x]3sV",roles:[{role:"dbOwner",db:"wdb"}]});
 db.createUser({user:"scheduler",pwd:"c93lbcp0hc[5209sebf10{3ca",roles:[{role:"read",db:"wdb"}]});
 db.fs.files.createIndex({md5 : 1, "metadata.property_id" : 1, "metadata.owner" : 1},{unique : true});
-db.executables.createIndex({name : 1, algorithm : 1, version : 1, build : 1, owner : 1},{unique : true});
+db.executables.createIndex({name : 1, algorithm : 1, version : 1, build : 1, owner : 1, params : 1},{unique : true});
 db.properties.createIndex({input_model_id : 1, executable_id : 1, md5 : 1, owner : 1},{unique : true});
 db.jobs.createIndex({name : 1, owner : 1, md5 : 1},{unique : true});
 db.collaborations.createIndex({name : 1},{unique : true});
