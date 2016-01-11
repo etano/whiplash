bc436ac48923aecafc745130fae8c8d3bd8dacd0
Ilia Zintchenko <zintchenko@itp.phys.ethz.ch>
2016-01-11 06:50:18 +0100

    non-unique index on status

--- odb/startup.js
+++ odb/startup.js
@@ -1,10 +1,11 @@
 try{ db.createUser({user:"www",pwd:"7cJgeAkHdw{oktPNYdgYE3nJ",roles:[{role:"readWrite",db:"wdb"}]}); } catch(err) {}
 try{ db.createUser({user:"api",pwd:"haYrv{Ak9UJiaDsqVTe7rLJTc",roles:[{role:"readWrite",db:"wdb"}]}); } catch(err) {}
 try{ db.createUser({user:"pwn",pwd:"cftXzdrjheHEARuJKT39x]3sV",roles:[{role:"dbOwner",db:"wdb"}]}); } catch(err) {}
-try{ db.createUser({user:"scheduler",pwd:"c93lbcp0hc[5209sebf10{3ca",roles:[{role:"read",db:"wdb"}]}); } catch(err) {}
+ntry{ db.createUser({user:"scheduler",pwd:"c93lbcp0hc[5209sebf10{3ca",roles:[{role:"read",db:"wdb"}]}); } catch(err) {}
 try{ db.fs.files.createIndex({md5 : 1, "metadata.property_id" : 1, "metadata.owner" : 1},{unique : true}); } catch(err) {}
 try{ db.executables.createIndex({name : 1, algorithm : 1, version : 1, build : 1, owner : 1},{unique : true}); } catch(err) {}
 try{ db.properties.createIndex({input_model_id : 1, executable_id : 1, md5 : 1, owner : 1},{unique : true}); } catch(err) {}
+try{ db.properties.createIndex({status : 1},{unique : false}); } catch(err) {}
 try{ db.jobs.createIndex({name : 1, owner : 1, md5 : 1},{unique : true}); } catch(err) {}
 try{ db.collaborations.createIndex({name : 1},{unique : true}); } catch(err) {}
 try{ db.users.createIndex({username : 1},{unique : true}); } catch(err) {}
