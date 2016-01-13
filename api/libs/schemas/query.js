df46e7f6645de4307c0c2037ec658a5dd7a15f79
Ethan Brown <ethan.w.brown@gmail.com>
2016-01-13 04:24:01 +0100

    forgot schema for query

---
+++ api/libs/schemas/query.js
@@ -0,0 +1,12 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+// Query
+var Query = new Schema({
+    owner: { type: String, required: true },
+    timestamp: { type: Date, default: Date.now, required : true },
+    filters: { type: Object, required: true },
+    fields: { type: Object, required: true }
+});
+
+module.exports = mongoose.model('Query', Query);
