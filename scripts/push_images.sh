cfaa1751a3fb05947c49b3b636b1bcbd9a515ee7
Ilia Zintchenko <zintchenko@itp.phys.ethz.ch>
2016-01-15 15:53:42 +0100

    clouding

--- scripts/push_images.sh
+++ scripts/push_images.sh
@@ -3,7 +3,7 @@
 cp python/whiplash.py scheduler/
 docker-compose -f cloud_build.yml build
 
-for image in "scheduler" "api" "odb" "www"
+for image in "scheduler" "api" "odb"
 do
     docker tag whiplash_${image}:latest whiplash/${image}:latest
     docker push "whiplash/${image}";
