8c15a4001b623f0d9e04369787f4cb5c7c7ba60b
Ilia Zintchenko <zintchenko@itp.phys.ethz.ch>
2016-01-15 13:38:32 +0100

    clouding

--- scripts/push_images.sh
+++ scripts/push_images.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 cp python/whiplash.py scheduler/
-docker-compose -f develop.yml build
+docker-compose -f cloud_build.yml build
 
 for image in "scheduler" "api" "odb" "www"
 do
-    docker tag whiplash_${image}_dev:latest whiplash/${image}:latest
+    docker tag whiplash_${image}:latest whiplash/${image}:latest
     docker push "whiplash/${image}";
 done
