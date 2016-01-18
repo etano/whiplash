0b1e314d100db021076153a38605c87cb05aa000
Ilia Zintchenko <zintchenko@itp.phys.ethz.ch>
2016-01-17 19:55:55 -0800

    clouding

--- scripts/cloud_deploy_ubuntu.sh
+++ scripts/cloud_deploy_ubuntu.sh
@@ -3,12 +3,13 @@
 ec2_host=$1
 
 ssh -t ${ec2_host} 'sudo apt-get update -y'
-ssh -t ${ec2_host} 'sudo yum install -y build-essential make docker.io g++ libboost-dev'
+ssh -t ${ec2_host} 'wget -qO- https://get.docker.com/ | sh'
+ssh -t ${ec2_host} 'sudo apt-get install -y build-essential make docker.io g++ libboost-dev'
 ssh -t ${ec2_host} 'sudo ln -sf /usr/bin/docker.io /usr/local/bin/docker'
-ssh -t ${ec2_host} 'sed -i '$acomplete -F _docker docker' /etc/bash_completion.d/docker.io'
+#ssh -t ${ec2_host} 'sed -i '$acomplete -F _docker docker' /etc/bash_completion.d/docker.io'
 (cd udyn && git checkout demo)
 scp -r udyn ${ec2_host}:
-ssh -t ${ec2_host} 'cd udyn && make'
+ssh -t ${ec2_host} 'cd udyn && make clean && make'
 ssh -t ${ec2_host} 'sudo service docker start'
 ssh -t ${ec2_host} 'sudo usermod -aG docker ubuntu'
 ssh -t ${ec2_host} 'curl -L https://github.com/docker/compose/releases/download/1.5.2/docker-compose-Linux-x86_64 > docker-compose'
