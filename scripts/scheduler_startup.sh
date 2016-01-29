35b4d033f21404d84e092f5b505cd55f52b8c10d
Ethan Brown <ethan.w.brown@gmail.com>
2016-01-29 11:50:18 +0100

    updated monch scripts to reflect changes

--- scripts/scheduler_startup.sh
+++ scripts/scheduler_startup.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-~/whiplash/scheduler/scheduler_users.py > /mnt/lnec/whiplash/logs/scheduler/users/log 2> /mnt/lnec/whiplash/logs/scheduler/users/log < /dev/null &
+/users/whiplash/whiplash/scheduler/users.py --cluster --host monchc300.cscs.ch --port 1337 --num_cpus 20 --log_dir /mnt/lnec/whiplash/logs/scheduler/users --scheduler_dir /mnt/lnec/whiplash/rte > /mnt/lnec/whiplash/logs/scheduler/users/log 2> /mnt/lnec/whiplash/logs/scheduler/users/log < /dev/null &
