726aa4823fbef2791353b0aeadb1fbcc02ea735f
Ilia Zintchenko <zintchenko@itp.phys.ethz.ch>
2016-02-04 17:58:59 +0100

    add authentication to big deploy

--- scripts/deploy_big.sh
+++ scripts/deploy_big.sh
@@ -4,10 +4,23 @@
 
 database_name="wdb"
 
+#data_dir="/mnt/lnec/whiplash/data"
 data_dir="data"
+
+#log_dir="/mnt/lnec/whiplash/logs/mongo"
 log_dir="log"
 
+key_file="mongodb.keyfile"
+
 sharded_collections=("properties" "executables")
+
+user_admin_username="UserAdmin"
+user_admin_password="pass0"
+
+root_admin_username="RootAdmin"
+root_admin_password="pass1"
+
+server_name="rs_conf"
 
 server_hosts=(127.0.0.1 127.0.0.1 127.0.0.1)
 server_ports=(27014 27015 27016)
@@ -26,6 +39,7 @@ replica_ports=(27018 27019 27020 27021 27022 27023 27024 27025 27026)
 shard_log_dir=${log_dir}/shards
 shard_data_dir=${data_dir}/shards
 
+#should be some of the replicas
 shard_hosts=(127.0.0.1 127.0.0.1 127.0.0.1)
 shard_ports=(27018 27021 27024)
 
@@ -44,10 +58,13 @@ sleep 3
 
 echo "Preparing"
 
-#rm -rf ${data_dir} ${log_dir}
+rm -rf ${data_dir} ${log_dir} #WARNING: debug
 
 mkdir -p ${data_dir}
 mkdir -p ${log_dir}
+
+openssl rand -base64 741 > ${key_file}
+chmod 600 ${key_file}
 
 #deploy servers
 
@@ -55,6 +72,36 @@ echo "Deploying servers"
 
 mkdir -p ${server_log_dir}
 mkdir -p ${server_data_dir}
+
+#set access control
+
+server_data_dir_n=${server_data_dir}/db_0
+mkdir -p ${server_data_dir_n}
+
+echo "processManagement:
+   fork: true
+systemLog:
+   destination: file
+   path: \"${server_log_dir}/0.o\"
+net:
+   bindIp: ${server_hosts[0]}
+   port: ${server_ports[0]}
+storage:
+   dbPath: \"${server_data_dir_n}\"
+   journal:
+      enabled: true" > mongo.config
+
+mongod --config mongo.config
+
+echo "db.createUser({user: \"${user_admin_username}\",pwd: \"${user_admin_password}\",roles: [ { role: \"userAdminAnyDatabase\", db: \"admin\"}]});" > config.js
+echo "db.createUser({user: \"${root_admin_username}\",pwd: \"${root_admin_password}\",roles: [ { role: \"root\", db: \"admin\"}]});" >> config.js
+mongo ${server_hosts[0]}:${server_ports[0]}/admin config.js
+
+kill $(cat ${server_data_dir_n}/mongod.lock)
+
+sleep 1
+
+#deploy
 
 for (( i=0; i<${#server_hosts[@]}; i++ ))
 do
@@ -69,10 +116,14 @@ systemLog:
 sharding:
    clusterRole: configsvr
 replication:
-   replSetName: configReplSet
+   replSetName: ${server_name}
 net:
    bindIp: ${server_hosts[${i}]}
    port: ${server_ports[${i}]}
+security:
+   authorization: enabled
+   clusterAuthMode: keyFile
+   keyFile: \"${key_file}\"
 storage:
    dbPath: \"${server_data_dir_n}\"
    journal:
@@ -93,10 +144,9 @@ do
     members=${members}"{_id:${i},\"host\":\"${server_hosts[${i}]}:${server_ports[${i}]}\"},"
 done
 members=${members%?}
-
-echo "rs.initiate({_id:\"configReplSet\",configsvr: true,members:[${members}]})" > config.js
-
-mongo ${server_hosts[0]}:${server_ports[0]} config.js
+echo "db.auth(\"${root_admin_username}\", \"${root_admin_password}\");" > config.js
+echo "rs.initiate({_id:\"${server_name}\",configsvr: true,members:[${members}]})" >> config.js
+mongo ${server_hosts[0]}:${server_ports[0]}/admin config.js
 
 sleep 1
 
@@ -106,7 +156,7 @@ echo "Deploying routers"
 
 mkdir -p ${router_log_dir}
 
-sharding="configReplSet/"
+sharding="${server_name}/"
 for (( i=0; i<${#server_hosts[@]}; i++ ))
 do
     sharding=${sharding}${server_hosts[${i}]}:${server_ports[${i}]},
@@ -124,6 +174,9 @@ systemLog:
 net:
    bindIp: ${router_hosts[${i}]}
    port: ${router_ports[${i}]}
+security:
+   clusterAuthMode: keyFile
+   keyFile: \"${key_file}\"
 sharding:
    configDB: ${sharding}" > mongo.config
 
@@ -133,9 +186,9 @@ done
 
 sleep 1
 
-#deploy replicas
+#deploy shards
 
-echo "Deploying replicas"
+echo "Deploying shards"
 
 mkdir -p ${shard_log_dir}
 mkdir -p ${shard_data_dir}
@@ -148,9 +201,40 @@ do
     shard_log_dir_n=${shard_log_dir}/${shard_names[${i}]}
     mkdir -p ${shard_log_dir_n}    
 
+    #set access control
+
+    replica_data_dir_n=${shard_data_dir_n}/db_0
+    mkdir -p ${replica_data_dir_n}
+
+    ind0=$(echo "${i}*${num_replicas}" | bc)
+
+    echo "processManagement:
+   fork: true
+systemLog:
+   destination: file
+   path: \"${shard_log_dir_n}/0.o\"
+net:
+   bindIp: ${replica_hosts[${ind0}]}
+   port: ${replica_ports[${ind0}]}
+storage:
+   dbPath: \"${replica_data_dir_n}\"
+   journal:
+      enabled: true" > mongo.config
+
+    mongod --config mongo.config
+
+    echo "db.createUser({user: \"${user_admin_username}\",pwd: \"${user_admin_password}\",roles: [ { role: \"userAdminAnyDatabase\", db: \"admin\"}]});" > config.js
+    echo "db.createUser({user: \"${root_admin_username}\",pwd: \"${root_admin_password}\",roles: [ { role: \"root\", db: \"admin\"}]});" >> config.js
+    mongo ${replica_hosts[${ind0}]}:${replica_ports[${ind0}]}/admin config.js
+
+    kill $(cat ${replica_data_dir_n}/mongod.lock)
+
+    sleep 1
+
+    #deploy
+
     for (( j=0; j<${num_replicas}; j++ ))
     do
-
         replica_data_dir_n=${shard_data_dir_n}/db_${j}
         mkdir -p ${replica_data_dir_n}
 
@@ -168,6 +252,10 @@ storage:
     dbPath: \"${replica_data_dir_n}\"
     journal:
         enabled: true
+security:
+    authorization: enabled
+    clusterAuthMode: keyFile
+    keyFile: \"${key_file}\"
 replication:
     replSetName: \"${shard_names[${i}]}\"" > mongo.config
 
@@ -181,10 +269,9 @@ replication:
         members=${members}"{_id:${j},\"host\":\"${replica_hosts[${ind}]}:${replica_ports[${ind}]}\"},"
     done
     members=${members%?}
-
-    echo "rs.initiate({_id:\"${shard_names[${i}]}\",members:[${members}]})" > config.js
-    ind0=$(echo "${i}*${num_replicas}" | bc)
-    mongo ${replica_hosts[${ind0}]}:${replica_ports[${ind0}]} config.js
+    echo "db.auth(\"${root_admin_username}\", \"${root_admin_password}\");" > config.js
+    echo "rs.initiate({_id:\"${shard_names[${i}]}\",members:[${members}]})" >> config.js
+    mongo ${replica_hosts[${ind0}]}:${replica_ports[${ind0}]}/admin config.js
 
 done
 
@@ -196,8 +283,9 @@ echo "Adding shards"
 
 for (( i=0; i<${#shard_hosts[@]}; i++ ))
 do
-    echo "sh.addShard(\"${shard_names[${i}]}/${shard_hosts[${i}]}:${shard_ports[${i}]}\")" > config.js
-    mongo ${router_hosts[0]}:${router_ports[0]} config.js
+    echo "db.auth(\"${root_admin_username}\", \"${root_admin_password}\");" > config.js
+    echo "sh.addShard(\"${shard_names[${i}]}/${shard_hosts[${i}]}:${shard_ports[${i}]}\")" >> config.js
+    mongo ${router_hosts[0]}:${router_ports[0]}/admin config.js
 done
 
 sleep 1
@@ -206,20 +294,23 @@ sleep 1
 
 echo "Enabling sharding"
 
-echo "sh.enableSharding(\"${database_name}\")" > config.js
-mongo ${router_hosts[0]}:${router_ports[0]} config.js
+echo "db.auth(\"${root_admin_username}\", \"${root_admin_password}\");" > config.js
+echo "sh.enableSharding(\"${database_name}\")" >> config.js
+mongo ${router_hosts[0]}:${router_ports[0]}/admin config.js
 
 sleep 1
 
 #shard collections
 
+echo "Sharding collections"
+
 for (( i=0; i<${#sharded_collections[@]}; i++ ))
 do
     collection=${sharded_collections[${i}]}
-    echo "Sharding ${collection}"
-    echo "db.${collection}.createIndex({_id:\"hashed\"})" > config.js
+    echo "db.auth(\"${root_admin_username}\", \"${root_admin_password}\");" > config.js
+    echo "db.${collection}.createIndex({_id:\"hashed\"})" >> config.js
     echo "sh.shardCollection(\"wdb.${collection}\",{_id:\"hashed\"})" >> config.js
-    mongo ${router_hosts[0]}:${router_ports[0]} config.js
+    mongo ${router_hosts[0]}:${router_ports[0]}/admin config.js
 done
 
 sleep 1
