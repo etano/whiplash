162aa8bb7b847d24144196ad5abf657e4d7b4400
Ilia Zintchenko <zintchenko@itp.phys.ethz.ch>
2016-02-08 14:04:42 +0100

    big deployment

--- scripts/deploy_big.sh
+++ scripts/deploy_big.sh
@@ -4,13 +4,13 @@
 
 database_name="wdb"
 
-#data_dir="/mnt/lnec/whiplash/data"
-data_dir="data"
+data_dir="/mnt/lnec/whiplash/data"
+#data_dirdata_dir="data"
 
-#log_dir="/mnt/lnec/whiplash/logs/mongo"
-log_dir="log"
+log_dir="/mnt/lnec/whiplash/logs/mongo"
+#log_dir="log"
 
-key_file="mongodb.keyfile"
+key_file="/mnt/lnec/whiplash/mongodb.keyfile"
 
 sharded_collections=("properties" "executables")
 
@@ -19,6 +19,9 @@ user_admin_password="pass0"
 
 root_admin_username="RootAdmin"
 root_admin_password="pass1"
+
+wdb_owner_username="pwn"
+wdb_owner_password="cftXzdrjheHEARuJKT39x]3sV"
 
 server_name="rs_conf"
 
@@ -48,17 +51,17 @@ shard_ports=(27018 27021 27024)
 
 #kill database
 
-echo "Killing database"
+#echo "Killing database"
 
-killall mongod
-killall mongos
-sleep 3
+#killall mongod
+#killall mongos
+#sleep 3
 
 #prepare
 
 echo "Preparing"
 
-rm -rf ${data_dir} ${log_dir} #WARNING: debug
+#rm -rf ${data_dir} ${log_dir} #WARNING: debug
 
 mkdir -p ${data_dir}
 mkdir -p ${log_dir}
@@ -93,9 +96,12 @@ storage:
 
 mongod --config mongo.config
 
+sleep 10
+
 echo "db.createUser({user: \"${user_admin_username}\",pwd: \"${user_admin_password}\",roles: [ { role: \"userAdminAnyDatabase\", db: \"admin\"}]});" > config.js
 echo "db.createUser({user: \"${root_admin_username}\",pwd: \"${root_admin_password}\",roles: [ { role: \"root\", db: \"admin\"}]});" >> config.js
 mongo ${server_hosts[0]}:${server_ports[0]}/admin config.js
+sleep 1
 
 kill $(cat ${server_data_dir_n}/mongod.lock)
 
@@ -129,10 +135,10 @@ storage:
    journal:
       enabled: true" > mongo.config
 
-    mongod --config mongo.config
+    numactl --interleave=all mongod --config mongo.config
 done
 
-sleep 1
+sleep 10
 
 #initiate replica set
 
@@ -147,7 +153,6 @@ members=${members%?}
 echo "db.auth(\"${root_admin_username}\", \"${root_admin_password}\");" > config.js
 echo "rs.initiate({_id:\"${server_name}\",configsvr: true,members:[${members}]})" >> config.js
 mongo ${server_hosts[0]}:${server_ports[0]}/admin config.js
-
 sleep 1
 
 #deploy routers
@@ -180,11 +185,11 @@ security:
 sharding:
    configDB: ${sharding}" > mongo.config
 
-mongos --config mongo.config
+numactl --interleave=all mongos --config mongo.config
 
 done
 
-sleep 1
+sleep 10
 
 #deploy shards
 
@@ -223,10 +228,13 @@ storage:
 
     mongod --config mongo.config
 
+    sleep 10
+
     echo "db.createUser({user: \"${user_admin_username}\",pwd: \"${user_admin_password}\",roles: [ { role: \"userAdminAnyDatabase\", db: \"admin\"}]});" > config.js
     echo "db.createUser({user: \"${root_admin_username}\",pwd: \"${root_admin_password}\",roles: [ { role: \"root\", db: \"admin\"}]});" >> config.js
     mongo ${replica_hosts[${ind0}]}:${replica_ports[${ind0}]}/admin config.js
-
+    sleep 1
+ 
     kill $(cat ${replica_data_dir_n}/mongod.lock)
 
     sleep 1
@@ -259,8 +267,10 @@ security:
 replication:
     replSetName: \"${shard_names[${i}]}\"" > mongo.config
 
-        mongod --config mongo.config
+       numactl --interleave=all mongod --config mongo.config
     done
+
+    sleep 10
 
     members=""
     for (( j=0; j<${num_replicas}; j++ ))
@@ -272,7 +282,7 @@ replication:
     echo "db.auth(\"${root_admin_username}\", \"${root_admin_password}\");" > config.js
     echo "rs.initiate({_id:\"${shard_names[${i}]}\",members:[${members}]})" >> config.js
     mongo ${replica_hosts[${ind0}]}:${replica_ports[${ind0}]}/admin config.js
-
+    sleep 1
 done
 
 sleep 1
@@ -286,9 +296,8 @@ do
     echo "db.auth(\"${root_admin_username}\", \"${root_admin_password}\");" > config.js
     echo "sh.addShard(\"${shard_names[${i}]}/${shard_hosts[${i}]}:${shard_ports[${i}]}\")" >> config.js
     mongo ${router_hosts[0]}:${router_ports[0]}/admin config.js
+    sleep 1
 done
-
-sleep 1
 
 #enable sharding
 
@@ -297,7 +306,6 @@ echo "Enabling sharding"
 echo "db.auth(\"${root_admin_username}\", \"${root_admin_password}\");" > config.js
 echo "sh.enableSharding(\"${database_name}\")" >> config.js
 mongo ${router_hosts[0]}:${router_ports[0]}/admin config.js
-
 sleep 1
 
 #shard collections
@@ -308,9 +316,54 @@ for (( i=0; i<${#sharded_collections[@]}; i++ ))
 do
     collection=${sharded_collections[${i}]}
     echo "db.auth(\"${root_admin_username}\", \"${root_admin_password}\");" > config.js
-    echo "db.${collection}.createIndex({_id:\"hashed\"})" >> config.js
-    echo "sh.shardCollection(\"wdb.${collection}\",{_id:\"hashed\"})" >> config.js
+    echo "db.getSiblingDB(\"${database_name}\").${collection}.createIndex({_id:\"hashed\"})" >> config.js
+    echo "sh.shardCollection(\"${database_name}.${collection}\",{_id:\"hashed\"})" >> config.js
     mongo ${router_hosts[0]}:${router_ports[0]}/admin config.js
+    sleep 10
 done
+
+echo "db.auth(\"${root_admin_username}\", \"${root_admin_password}\");" > config.js
+echo "sh.shardCollection(\"${database_name}.fs.chunks\",{file_id: 1})" >> config.js
+mongo ${router_hosts[0]}:${router_ports[0]}/admin config.js
+sleep 10
+
+#add users
+
+echo "Adding users"
+
+echo "db.auth(\"${user_admin_username}\", \"${user_admin_password}\");" > config.js
+echo "db.getSiblingDB(\"${database_name}\").createUser({user:\"www\",pwd:\"7cJgeAkHdw{oktPNYdgYE3nJ\",roles:[{role:\"readWrite\",db:\"wdb\"}]});" >> config.js
+echo "db.getSiblingDB(\"${database_name}\").createUser({user:\"api\",pwd:\"haYrv{Ak9UJiaDsqVTe7rLJTc\",roles:[{role:\"readWrite\",db:\"wdb\"}]});" >> config.js
+echo "db.getSiblingDB(\"${database_name}\").createUser({user:\"${wdb_owner_username}\",pwd:\"${wdb_owner_password}\",roles:[{role:\"dbOwner\",db:\"wdb\"}]});" >> config.js
+echo "db.getSiblingDB(\"${database_name}\").createUser({user:\"scheduler\",pwd:\"c93lbcp0hc[5209sebf10{3ca\",roles:[{role:\"read\",db:\"wdb\"}]});" >> config.js
+mongo ${router_hosts[0]}:${router_ports[0]}/admin config.js
+
+sleep 10
+
+#add indexes
+
+echo "Adding indexes"
+
+echo "db.auth(\"${wdb_owner_username}\", \"${wdb_owner_password}\");" > config.js
+
+#unique indexes
+
+echo "db.fs.files.createIndex({\"metadata.md5\" : 1, \"metadata.property_id\" : 1, \"metadata.owner\" : 1},{unique : true});" >> config.js
+echo "db.executables.createIndex({name: 1, path: 1, md5: 1, owner: 1},{unique: true});" >> config.js
+echo "db.properties.createIndex({md5: 1},{unique: true});" >> config.js
+echo "db.queries.createIndex({owner: 1, md5: 1},{unique: true});" >> config.js
+echo "db.collaborations.createIndex({name: 1},{unique: true});" >> config.js
+echo "db.users.createIndex({username: 1},{unique: true});" >> config.js
+echo "db.clients.createIndex({name: 1},{unique: true});" >> config.js
+
+#speed indexes
+
+echo "db.work_batches.createIndex({timestamp : 1},{unique: false});" >> config.js
+echo "db.properties.createIndex({owner: 1, status: 1},{unique: false});" >> config.js
+echo "db.properties.createIndex({commit_tag: 1},{unique: false});" >> config.js
+echo "db.properties.createIndex({status: 1, timeout: 1},{unique: false});" >> config.js
+echo "db.properties.createIndex({input_model_id: 1, executable_id: 1},{unique: false});" >> config.js
+
+mongo ${router_hosts[0]}:${router_ports[0]}/wdb config.js
 
 sleep 1
