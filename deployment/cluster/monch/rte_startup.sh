3056cfdd8b17abd2598c38bcecbead4cca7217c5
Ethan Brown <ethan.w.brown@gmail.com>
2016-03-22 19:06:01 +0100

    fixed error in rte_startup.sh

--- deployment/cluster/monch/rte_startup.sh
+++ deployment/cluster/monch/rte_startup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-cp $WHIPLASH_HOME/rte/*.py /mnt/lnec/$USER/rte/
-/mnt/lnec/$USER/whiplash/manager.py --cluster --host ${HOSTNAME}.cscs.ch --port $NODE_PORT --num_cpus 20 --log_dir /mnt/lnec/$USER/whiplash/logs/rte/ --rte_dir /mnt/lnec/$USER/whiplash/rte > /mnt/lnec/$USER/whiplash/logs/rte/manager.log 2> /mnt/lnec/$USER/whiplash/logs/rte/manager.log < /dev/null &
+cp $WHIPLASH_HOME/rte/*.py /mnt/lnec/$USER/whiplash/rte/
+/mnt/lnec/$USER/whiplash/rte/manager.py --cluster --host ${HOSTNAME}.cscs.ch --port $NODE_PORT --num_cpus 20 --log_dir /mnt/lnec/$USER/whiplash/logs/rte/ --rte_dir /mnt/lnec/$USER/whiplash/rte > /mnt/lnec/$USER/whiplash/logs/rte/manager.log 2> /mnt/lnec/$USER/whiplash/logs/rte/manager.log < /dev/null &
 disown
