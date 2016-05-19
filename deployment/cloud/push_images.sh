aeb8fa490167afdff8947631cbd6ce1c21a36b2c
Ethan Brown <ethan.w.brown@gmail.com>
2016-05-19 16:04:19 +0200

    added push_images.sh

--- deployment/cloud/push_images.sh
+++ deployment/cloud/push_images.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for image in "odb" "api" "rte"
+for image in "api" "rte" "www"
 do
     docker-compose -f ../../${image}/docker-compose.yml build
     docker tag -f ${image}_${image}:latest whiplash/${image}:latest
