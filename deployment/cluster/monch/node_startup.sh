7b3e43736a643e587e1af82180b44ccaea62e2ae
Ethan Brown <ethan.w.brown@gmail.com>
2016-05-20 16:00:41 +0200

    slight optimization, bug fix in cluster deployment

--- deployment/cluster/monch/node_startup.sh
+++ deployment/cluster/monch/node_startup.sh
@@ -4,6 +4,6 @@ module load gcc/4.8.1
 cd $WHIPLASH_HOME/api
 npm install
 node --use_strict bin/init_db
-node --use_strict bin/create_user admin
+node --use_strict bin/create_user admin $ADMIN_PASSWORD admin@whiplash.ethz.ch
 node --use_strict bin/create_user $USER $USER $USER@itp.phys.ethz.ch
 node --use_strict bin/api > /dev/null 2>&1 &
