365d80303ef828061ec2a00d1719e14d2dfa3a60
Ethan Brown <ethan.w.brown@gmail.com>
2016-06-01 15:47:16 +0200

    changed deployment scripts

--- deployment/cloud/amazon/launch_api.sh
+++ deployment/cloud/amazon/launch_api.sh
@@ -1,17 +1,22 @@
 #!/bin/bash
 
-source settings.sh
+{
 docker-machine create --driver amazonec2 \
-                      --amazonec2-security-group whiplash-api \
+                      --amazonec2-security-group WhiplashApi \
                       --amazonec2-access-key ${AWS_ACCESS_KEY} \
                       --amazonec2-secret-key ${AWS_SECRET_KEY} \
-                      --amazonec2-region eu-central-1 \
-                      aws-api
-eval $(docker-machine env aws-api)
+                      --amazonec2-vpc-id ${AWS_VPC_ID} \
+                      --amazonec2-region eu-west-1 \
+                      whiplash-api-0
+} && {
+eval $(docker-machine env whiplash-api-0)
+} && {
 docker run -d \
            --name "whiplash-api" \
            -e "MONGO_PORT_27017_TCP_ADDR=${MONGO_PORT_27017_TCP_ADDR}" \
            -e "MONGO_PORT_27017_TCP_PORT=${MONGO_PORT_27017_TCP_PORT}" \
            -e "MONGO_API_PASSWORD=${MONGO_API_PASSWORD}" \
            -p 1337:1337 \
-           whiplash/api sh -c "node --use_strict bin/init_db; node --use_strict bin/create_user admin ${WHIPLASH_ADMIN_PASSWORD} admin@whiplash.ethz.ch; node --use_strict bin/create_user test test test@test.com; node --use_strict bin/api"
+           --restart=always \
+           whiplash/api sh -c "node --use_strict bin/init_db; node --use_strict bin/create_user admin ${WHIPLASH_ADMIN_PASSWORD} ${WHIPLASH_ADMIN_EMAIL}; node --use_strict bin/api"
+}
--- deployment/cloud/amazon/launch_scheduler.sh
+++ deployment/cloud/amazon/launch_scheduler.sh
@@ -1,22 +1,26 @@
 #!/bin/bash
 
-source settings.sh
+{
 docker-machine create --driver amazonec2 \
-                      --amazonec2-security-group whiplash-scheduler \
+                      --amazonec2-security-group WhiplashScheduler \
                       --amazonec2-access-key ${AWS_ACCESS_KEY} \
                       --amazonec2-secret-key ${AWS_SECRET_KEY} \
-                      --amazonec2-region eu-central-1 \
-                      aws-scheduler
-eval $(docker-machine env aws-scheduler)
+                      --amazonec2-vpc-id ${AWS_VPC_ID} \
+                      --amazonec2-region eu-west-1 \
+                      whiplash-scheduler-0
+} && {
+eval $(docker-machine env whiplash-scheduler-0)
+} && {
 docker run -d \
            --name "whiplash-scheduler" \
            -v /var/run/docker.sock:/var/run/docker.sock \
-           -v /usr/bin/docker:/bin/docker \
            -v $PWD:/input \
            -e "WHIPLASH_ADMIN_PASSWORD=${WHIPLASH_ADMIN_PASSWORD}" \
            -e "WHIPLASH_HOST_WORK_DIR=$PWD" \
-           -e "WHIPLASH_LAUNCH_WORK_DIR=\"/input\"" \
-           -e "WHIPLASH_API_HOST=$(docker-machine ip aws-api)" \
+           -e "WHIPLASH_LAUNCH_WORK_DIR=\/input" \
+           -e "WHIPLASH_API_HOST=$(docker-machine ip whiplash-api-0)" \
            -e "WHIPLASH_API_PORT=1337" \
            -p 1337:1337 \
+           --restart=always \
            whiplash/scheduler sh -c "node --use_strict bin/scheduler"
+}
