25506896529fb06827468b69060cdb832f622031
Ethan Brown <ewb@ewb.io>
2016-09-24 11:49:56 +0000

    removed scruff

--- deployment/cloud/push_images.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-for image in "api" "scheduler" "www"
-do
-    docker-compose -f ../../${image}/docker-compose.yml build
-    docker tag ${image}_${image}:latest whiplash/${image}:latest
-    docker push "whiplash/${image}";
-done
